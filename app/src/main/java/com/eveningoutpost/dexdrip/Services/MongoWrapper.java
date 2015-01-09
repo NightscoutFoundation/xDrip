b6cb080e9a76f3d48331c05a7a4fbd5bf89fac4c
Tzachi Dar <tzachi.dar@gmail.com>
2015-01-09 11:31:29 +0200

    Allow the Dexdrip to also read from a mongodb.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/Services/MongoWrapper.java
@@ -0,0 +1,149 @@
+package com.eveningoutpost.dexdrip.Services;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.MongoClientURI;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class MongoWrapper {
+	
+	MongoClient mongoClient_;
+	String dbUriStr_;
+	String dbName_;
+	String collection_;
+	String index_;
+	String machineName_;
+	
+	public MongoWrapper(String dbUriStr, String collection, String index, String machineName) {
+		dbUriStr_ = dbUriStr;
+		// dbName is the last part of the string starting with /dbname
+		dbName_ = dbUriStr.substring(dbUriStr.lastIndexOf('/') + 1);
+		collection_ = collection;
+		index_ = index;
+		machineName_ = machineName;
+	}
+	
+	// Unfortunately, this also throws other exceptions that are not documetned...
+    public DBCollection openMongoDb() throws UnknownHostException {
+
+    	MongoClientURI dbUri = new MongoClientURI(dbUriStr_); //?? thros
+	    mongoClient_ = new MongoClient(dbUri);
+	    
+	    DB db = mongoClient_.getDB( dbName_ );
+	    DBCollection coll = db.getCollection(collection_);
+	    coll.createIndex(new BasicDBObject(index_, 1));  // create index on "i", ascending
+	    
+	    return coll;
+	 
+    }
+     
+     public void closeMongoDb() {
+         if(mongoClient_ != null) {
+    	 	mongoClient_.close();
+         }
+     }
+
+     public boolean WriteDebugDataToMongo(String message)
+     {
+    	 String complete = machineName_ + " " + new Date().toLocaleString() + " " + message;
+    	 BasicDBObject doc = new BasicDBObject("DebugMessage", complete);
+    	 return WriteToMongo(doc);
+     }
+
+     
+     public boolean WriteToMongo(TransmitterRawData trd)
+     {
+    	 BasicDBObject bdbo = trd.toDbObj(machineName_ + " " + new Date(trd.CaptureDateTime).toLocaleString());
+    	 return WriteToMongo(bdbo);
+     }
+     
+     public boolean WriteToMongo(BasicDBObject bdbo)
+     {
+     	DBCollection coll;
+     	try {
+     		coll = openMongoDb();
+         	coll.insert(bdbo);
+
+ 		} catch (UnknownHostException e) {
+ 			e.printStackTrace();
+ 			return false; 
+ 		} catch (MongoException e) {
+ 			e.printStackTrace();
+ 			return false; 
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 			closeMongoDb();
+ 			return false; 
+ 		}
+     	finally {
+ 			closeMongoDb();
+ 		}
+     	return true;
+     }
+     
+     // records will be marked by their timestamp
+     public List<TransmitterRawData> ReadFromMongo(int numberOfRecords) {
+    	System.out.println( "Starting to read from mongodb"); 
+    	 
+    	List<TransmitterRawData> trd_list = new LinkedList<TransmitterRawData>();
+      	DBCollection coll;
+      	TransmitterRawData lastTrd = null;
+      	try {
+      		coll = openMongoDb();
+      		DBCursor cursor = coll.find();
+            cursor.sort(new BasicDBObject("CaptureDateTime", -1));
+            try {
+                while(cursor.hasNext() && trd_list.size() < numberOfRecords) {
+                    //System.out.println(cursor.next());
+                    TransmitterRawData trd = new TransmitterRawData((BasicDBObject)cursor.next());
+                    // Do our best to fix the relative time...
+                    trd.RelativeTime = new Date().getTime() - trd.CaptureDateTime;
+                    // since we are reading it from the db, it was uploaded...
+                    trd.Uploaded = 1;
+                    if(lastTrd == null) {
+                    	trd_list.add(0,trd);
+                    	lastTrd = trd;
+                    	System.out.println( trd.toTableString());
+                    } else if(!WixelReader.almostEquals(lastTrd, trd)) {
+                    	lastTrd = trd;
+                    	trd_list.add(0,trd);
+                    	System.out.println( trd.toTableString());
+                    }
+                    
+                }
+             } finally {
+                cursor.close();
+             }
+
+  		} catch (UnknownHostException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  			return null; 
+  		} catch (MongoException e) {
+  			// TODO Auto-generated catch block
+  			e.printStackTrace();
+  			return trd_list; 
+  		} catch (Exception e) {
+ 			e.printStackTrace();
+ 			closeMongoDb();
+ 			return null; 
+ 		}finally {
+  			closeMongoDb();
+  		}
+      	return trd_list;
+    	 
+     }
+
+
+}
