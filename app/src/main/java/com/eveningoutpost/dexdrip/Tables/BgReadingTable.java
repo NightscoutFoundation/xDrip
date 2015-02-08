b5206ccd8c7e246082f5946bede47d4e38442d81
Benny Handfast <benny.github@handfast.se>
2015-02-08 22:32:14 +0100

    Remove cursor leaks.

--- app/src/main/java/com/eveningoutpost/dexdrip/Tables/BgReadingTable.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Tables/BgReadingTable.java
@@ -5,25 +5,25 @@ import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
-import android.widget.SimpleCursorAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
-import com.activeandroid.Cache;
 import com.eveningoutpost.dexdrip.Models.BgReading;
 import com.eveningoutpost.dexdrip.NavigationDrawerFragment;
 import com.eveningoutpost.dexdrip.R;
 
 import java.util.Date;
+import java.util.List;
 
 
 public class BgReadingTable extends ListActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
     private String menu_name = "BG Data Table";
     private NavigationDrawerFragment mNavigationDrawerFragment;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,32 +31,25 @@ public class BgReadingTable extends ListActivity implements NavigationDrawerFrag
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
         mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), menu_name, this);
 
         getData();
     }
+
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         mNavigationDrawerFragment.swapContext(position);
     }
 
     private void getData() {
-        Cursor cursor = Cache.openDatabase().rawQuery("Select * from BgReadings order by _ID desc limit 50", null);
-
-//        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-//                R.layout.raw_data_list_item,
-//                cursor,
-//                new String[] { "calculated_value", "age_adjusted_raw_value", "raw_data", "time_since_sensor_started" },
-//                new int[] { R.id.raw_data_id, R.id.raw_data_value , R.id.raw_data_slope, R.id.raw_data_timestamp });
-
-        CursorAdapter adapter = new BgReadingCursorAdapter(this, cursor, 0);
+        final List<BgReading> latest = BgReading.latest(50);
+        ListAdapter adapter = new BgReadingAdapter(this, latest);
 
         this.setListAdapter(adapter);
     }
-
 
     public static class BgReadingCursorAdapterViewHolder {
         TextView raw_data_id;
@@ -72,15 +65,17 @@ public class BgReadingTable extends ListActivity implements NavigationDrawerFrag
         }
     }
 
-    public static class BgReadingCursorAdapter extends CursorAdapter {
-        BgReading bgReading = new BgReading();
+    public static class BgReadingAdapter extends BaseAdapter {
+        private final Context         context;
+        private final List<BgReading> readings;
 
-        public BgReadingCursorAdapter(Context context, Cursor c, int flags) {
-            super(context, c, flags);
+        public BgReadingAdapter(Context context, List<BgReading> readings) {
+            this.context = context;
+
+            this.readings = readings;
         }
 
-        @Override
-        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        public View newView(Context context, ViewGroup parent) {
             final View view = LayoutInflater.from(context).inflate(R.layout.raw_data_list_item, parent, false);
 
             final BgReadingCursorAdapterViewHolder holder = new BgReadingCursorAdapterViewHolder(view);
@@ -89,15 +84,36 @@ public class BgReadingTable extends ListActivity implements NavigationDrawerFrag
             return view;
         }
 
-        @Override
-        public void bindView(View view, Context context, Cursor cursor) {
-            bgReading.loadFromCursor(cursor);
-
+        public void bindView(View view, Context context, BgReading bgReading) {
             final BgReadingCursorAdapterViewHolder tag = (BgReadingCursorAdapterViewHolder) view.getTag();
             tag.raw_data_id.setText(Double.toString(bgReading.calculated_value));
             tag.raw_data_value.setText(Double.toString(bgReading.age_adjusted_raw_value));
             tag.raw_data_slope.setText(Double.toString(bgReading.raw_data));
             tag.raw_data_timestamp.setText(new Date(bgReading.timestamp).toString());
+        }
+
+        @Override
+        public int getCount() {
+            return readings.size();
+        }
+
+        @Override
+        public BgReading getItem(int position) {
+            return readings.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return getItem(position).getId();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = newView(context, parent);
+
+            bindView(convertView, context, getItem(position));
+            return convertView;
         }
     }
 }
--- app/src/main/java/com/eveningoutpost/dexdrip/Tables/CalibrationDataTable.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Tables/CalibrationDataTable.java
@@ -8,7 +8,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.activeandroid.Cache;
@@ -17,14 +17,12 @@ import com.eveningoutpost.dexdrip.NavigationDrawerFragment;
 import com.eveningoutpost.dexdrip.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class CalibrationDataTable extends ListActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
     private String menu_name = "Calibration Data Table";
     private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    private ArrayList<String> results = new ArrayList<String>();
-    private View mRootView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,19 +44,11 @@ public class CalibrationDataTable extends ListActivity implements NavigationDraw
     }
 
     private void getData() {
-        Cursor cursor = Cache.openDatabase().rawQuery("Select * from Calibration order by timestamp desc", null);
+        final List<Calibration> latest = Calibration.latest(50);
 
-//        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-//                R.layout.raw_data_list_item,
-//                cursor,
-//                new String[] { "bg", "estimate_raw_at_time_of_calibration", "slope", "intercept" },
-//                new int[] { R.id.raw_data_id, R.id.raw_data_value , R.id.raw_data_slope, R.id.raw_data_timestamp });
-
-        CalibrationDataCursorAdapter adapter = new CalibrationDataCursorAdapter(this, cursor, 0);
+        CalibrationDataCursorAdapter adapter = new CalibrationDataCursorAdapter(this, latest);
 
         this.setListAdapter(adapter);
-//        ListView listView = (ListView) findViewById(R.id.list);
-//        listView.setAdapter(adapter);
     }
 
 
@@ -76,15 +66,16 @@ public class CalibrationDataTable extends ListActivity implements NavigationDraw
         }
     }
 
-    public static class CalibrationDataCursorAdapter extends CursorAdapter {
-        Calibration bgReading = new Calibration();
+    public static class CalibrationDataCursorAdapter extends BaseAdapter {
+        private final Context           context;
+        private final List<Calibration> calibrations;
 
-        public CalibrationDataCursorAdapter(Context context, Cursor c, int flags) {
-            super(context, c, flags);
+        public CalibrationDataCursorAdapter(Context context, List<Calibration> calibrations) {
+            this.context = context;
+            this.calibrations = calibrations;
         }
 
-        @Override
-        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        public View newView(Context context, ViewGroup parent) {
             final View view = LayoutInflater.from(context).inflate(R.layout.raw_data_list_item, parent, false);
 
             final CalibrationDataCursorAdapterViewHolder holder = new CalibrationDataCursorAdapterViewHolder(view);
@@ -93,15 +84,36 @@ public class CalibrationDataTable extends ListActivity implements NavigationDraw
             return view;
         }
 
-        @Override
-        public void bindView(View view, Context context, Cursor cursor) {
-            bgReading.loadFromCursor(cursor);
-
+        public void bindView(View view, Context context, Calibration calibration) {
             final CalibrationDataCursorAdapterViewHolder tag = (CalibrationDataCursorAdapterViewHolder) view.getTag();
-            tag.raw_data_id.setText(Double.toString(bgReading.bg));
-            tag.raw_data_value.setText(Double.toString(bgReading.estimate_raw_at_time_of_calibration));
-            tag.raw_data_slope.setText(Double.toString(bgReading.slope));
-            tag.raw_data_timestamp.setText(Double.toString(bgReading.intercept));
+            tag.raw_data_id.setText(Double.toString(calibration.bg));
+            tag.raw_data_value.setText(Double.toString(calibration.estimate_raw_at_time_of_calibration));
+            tag.raw_data_slope.setText(Double.toString(calibration.slope));
+            tag.raw_data_timestamp.setText(Double.toString(calibration.intercept));
+        }
+
+        @Override
+        public int getCount() {
+            return calibrations.size();
+        }
+
+        @Override
+        public Calibration getItem(int position) {
+            return calibrations.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return getItem(position).getId();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null)
+                convertView = newView(context, parent);
+
+            bindView(convertView, context, getItem(position));
+            return convertView;
         }
     }
 }
