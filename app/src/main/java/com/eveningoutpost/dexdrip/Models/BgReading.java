c610e76c58f426028b8970a7cf9bd787b01a921d
Stephen <stephen@shopkeep.com>
2015-06-10 13:20:50 -0400

    fix not setting noise and other issue I created, woops

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/BgReading.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/BgReading.java
@@ -218,14 +218,13 @@ public class BgReading extends Model {
                 double calSlope = (calibration.first_scale / firstAdjSlope)*1000;
                 double calIntercept = ((calibration.first_scale * calibration.first_intercept) / firstAdjSlope)*-1;
                 bgReading.raw_calculated = (((calSlope * bgReading.raw_data) + calIntercept) - 5);
-                bgReading.noise = egvRecord.noiseValue();
             }
             Log.w(TAG, "create: NEW VALUE CALCULATED AT: " + bgReading.calculated_value);
             bgReading.calculated_value_slope = bgReading.slopefromName(egvRecord.getTrend().friendlyTrendName());
+            bgReading.noise = egvRecord.noiseValue();
             if(egvRecord.getTrend().friendlyTrendName().compareTo("NOT_COMPUTABLE") == 0 || egvRecord.getTrend().friendlyTrendName().compareTo("OUT_OF_RANGE") == 0) {
                 bgReading.hide_slope = true;
             }
-
             bgReading.save();
             bgReading.find_new_curve();
             bgReading.find_new_raw_curve();
