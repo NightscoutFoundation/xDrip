t1djoe/xDrip-Experimental

Collect sensor location (take 2).
Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

app/src/main/java/com/eveningoutpost/dexdrip/StopSensor.java
@@ -44,9 +44,11 @@ public class StopSensor extends ActivityWithMenu {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Sensor sensor = Sensor.currentSensor();
-                sensor.stopped_at = new Date().getTime();
-                Log.w("NEW SENSOR", "Sensor stopped at " + sensor.stopped_at);
-                sensor.save();
+                if(sensor != null) {
+                    sensor.stopped_at = new Date().getTime();
+                    Log.w("NEW SENSOR", "Sensor stopped at " + sensor.stopped_at);
+                    sensor.save();
+                }
                 AlertPlayer.getPlayer().stopAlert(getApplicationContext(),true, false);
 
                 Toast.makeText(getApplicationContext(), "Sensor stopped", Toast.LENGTH_LONG).show();

NightscoutFoundation/xDrip

Conflicts:
	app/src/main/java/com/eveningoutpost/dexdrip/utils/Preferences.java
	app/src/main/res/values/strings.xml
	app/src/main/java/com/eveningoutpost/dexdrip/Sensor.java
	app/src/main/java/com/eveningoutpost/dexdrip/StartNewSensor.java
	app/src/main/java/com/eveningoutpost/dexdrip/StopSensor.java

app/src/main/java/com/eveningoutpost/dexdrip/StopSensor.java
@@ -45,6 +45,11 @@ public class StopSensor extends ActivityWithMenu {
                 sensor.stopped_at = new Date().getTime();
                 Log.i("NEW SENSOR", "Sensor stopped at " + sensor.stopped_at);
                 sensor.save();
+                if(sensor != null) {
+                    sensor.stopped_at = new Date().getTime();
+                    Log.i("NEW SENSOR", "Sensor stopped at " + sensor.stopped_at);
+                    sensor.save();
+                }
                 AlertPlayer.getPlayer().stopAlert(getApplicationContext(),true, false);
 
                 Toast.makeText(getApplicationContext(), "Sensor stopped", Toast.LENGTH_LONG).show();