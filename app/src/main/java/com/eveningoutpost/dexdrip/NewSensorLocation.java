28dd4ddc901aa6c5e3dbf996cd3101ba96af23bc
Tzachi Dar <tzachi.dar@gmail.com>
2015-09-18 09:55:52 +0300

    Fix typos.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- app/src/main/java/com/eveningoutpost/dexdrip/NewSensorLocation.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/NewSensorLocation.java
@@ -64,13 +64,13 @@ public class NewSensorLocation extends ActivityWithMenu {
         
         locations = new LinkedList<Location>();
         
-        locations.add(new Location("I dont wish to share", PRIVATE_ID));
+        locations.add(new Location("I don't wish to share", PRIVATE_ID));
         locations.add(new Location("Upper arm", 1));
         locations.add(new Location("Thigh", 2));
         locations.add(new Location("Belly (abdomen)", 3));
         locations.add(new Location("Lower back", 4));
-        locations.add(new Location("buttocks", 5));
-        locations.add(new Location("other", OTHER_ID));
+        locations.add(new Location("Buttocks", 5));
+        locations.add(new Location("Other", OTHER_ID));
         
         for(Location location : locations) {
             AddButton(location.location, location.location_id);
