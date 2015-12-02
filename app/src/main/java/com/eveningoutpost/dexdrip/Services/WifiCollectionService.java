068f6b76549ad594c0f2ef08ed884b29d43896a0
Jamorham <jamorham@users.noreply.github.com>
2015-12-02 08:51:59 +0000

    Add support for new collection service

--- app/src/main/java/com/eveningoutpost/dexdrip/Services/WifiCollectionService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Services/WifiCollectionService.java
@@ -55,7 +55,8 @@ public class WifiCollectionService extends Service {
             stopSelf();
             return START_NOT_STICKY;
         }
-        if (CollectionServiceStarter.isWifiWixel(getApplicationContext()) ) {
+        if (CollectionServiceStarter.isWifiWixel(getApplicationContext()) || CollectionServiceStarter.isWifiandBTWixel(getApplicationContext()) )
+        {
             runWixelReader();
             // For simplicity done here, would better happen once we know if we have a packet or not...
             setFailoverTimer();
@@ -94,7 +95,7 @@ public class WifiCollectionService extends Service {
 
 
     public void setFailoverTimer() {
-        if (CollectionServiceStarter.isWifiWixel(getApplicationContext())) {
+        if (CollectionServiceStarter.isWifiWixel(getApplicationContext()) || CollectionServiceStarter.isWifiandBTWixel(getApplicationContext())) {
             long retry_in = WixelReader.timeForNextRead();
             Log.d(TAG, "setFailoverTimer: Fallover Restarting in: " + (retry_in / (60 * 1000)) + " minutes");
             Calendar calendar = Calendar.getInstance();
