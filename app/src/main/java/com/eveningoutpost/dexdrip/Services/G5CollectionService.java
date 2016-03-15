f44d2de84fb93603684e9759c3bbbe1eef5de29c
Costik, John <John_Costik@URMC.Rochester.edu>
2016-03-15 13:28:51 -0400

    basic stubs of classes, and code updates to get rolling on the G5 integration
    Conflicts:
    	wear/wear.iml

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/Services/G5CollectionService.java
@@ -0,0 +1,7 @@
+package com.eveningoutpost.dexdrip.Services;
+
+/**
+ * Created by jcostik1 on 3/15/16.
+ */
+public class G5CollectionService {
+}
---
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/DexG5Attributes.java
@@ -0,0 +1,7 @@
+package com.eveningoutpost.dexdrip.UtilityModels;
+
+/**
+ * Created by jcostik1 on 3/15/16.
+ */
+public class DexG5Attributes {
+}
