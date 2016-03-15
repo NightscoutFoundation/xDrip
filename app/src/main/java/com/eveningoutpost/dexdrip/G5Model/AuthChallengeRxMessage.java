99cfaf40c4801369a8267424fa016e25e6d37349
Costik, John <John_Costik@URMC.Rochester.edu>
2016-03-15 14:43:11 -0400

    AuthChallenge wip

--- app/src/main/java/com/eveningoutpost/dexdrip/G5Model/AuthChallengeRxMessage.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/G5Model/AuthChallengeRxMessage.java
@@ -1,7 +1,21 @@
 package com.eveningoutpost.dexdrip.G5Model;
 
+import java.nio.ByteBuffer;
+
 /**
  * Created by jcostik1 on 3/15/16.
  */
 public class AuthChallengeRxMessage extends TransmitterRxMessage {
+    static byte opcode = 0x3;
+    ByteBuffer tokenHash = null;
+    ByteBuffer challenge = null;
+
+    AuthChallengeRxMessage(ByteBuffer data) {
+        if (data.array().length >= 17) {
+            if (data.array()[0] == opcode + 1 ) {
+                //challenge =
+                //tokenHash =
+            }
+        }
+    }
 }
