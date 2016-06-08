40e264a63c972663ec0cf397e596c785c13dc583
Jamorham <jamorham@users.noreply.github.com>
2016-06-08 09:50:00 +0100

    Wakelock adjustments and failover timer for follower donothing service

--- app/src/main/java/com/eveningoutpost/dexdrip/Services/DoNothingService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Services/DoNothingService.java
@@ -1,17 +1,27 @@
 package com.eveningoutpost.dexdrip.Services;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+import com.eveningoutpost.dexdrip.GcmActivity;
+import com.eveningoutpost.dexdrip.GcmListenerSvc;
+import com.eveningoutpost.dexdrip.Home;
+import com.eveningoutpost.dexdrip.Models.JoH;
 import com.eveningoutpost.dexdrip.Models.UserError;
 import com.eveningoutpost.dexdrip.UtilityModels.CollectionServiceStarter;
 import com.eveningoutpost.dexdrip.UtilityModels.ForegroundServiceStarter;
+import com.google.android.gms.gcm.GcmReceiver;
+
+import java.util.Calendar;
 
 public class DoNothingService extends Service {
     private final static String TAG = DoNothingService.class.getSimpleName();
@@ -57,17 +67,32 @@ public class DoNothingService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        final PowerManager.WakeLock wl = JoH.getWakeLock("donothing-follower", 60000);
         if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
             stopSelf();
+            JoH.releaseWakeLock(wl);
             return START_NOT_STICKY;
         }
         if (CollectionServiceStarter.isFollower(getApplicationContext())) {
-            Log.d(TAG, "Tick");
+            int minsago = GcmListenerSvc.lastMessageMinutesAgo();
+            Log.d(TAG, "Tick: minutes ago: " + minsago);
+            if (minsago > 6) {
+                GcmActivity.requestPing();
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+
+            setFailOverTimer();
         } else {
             stopSelf();
+            JoH.releaseWakeLock(wl);
             return START_NOT_STICKY;
         }
+        JoH.releaseWakeLock(wl);
         return START_STICKY;
+
     }
 
     @Override
@@ -76,6 +101,25 @@ public class DoNothingService extends Service {
         UserError.Log.d(TAG, "onDestroy entered");
         foregroundServiceStarter.stop();
         UserError.Log.i(TAG, "SERVICE STOPPED");
+    }
+
+    private void setFailOverTimer() {
+            if (Home.get_follower()) {
+                final long retry_in = (5*60*1000);
+                UserError.Log.d(TAG, "setFailoverTimer: Restarting in: " + (retry_in / (60 * 1000)) + " minutes");
+                final Calendar calendar = Calendar.getInstance();
+                final AlarmManager alarm = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    alarm.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() + retry_in, PendingIntent.getService(this, 0, new Intent(this, DoNothingService.class), 0));
+                } else if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                    alarm.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() + retry_in, PendingIntent.getService(this, 0, new Intent(this, DoNothingService.class), 0));
+                } else {
+                    alarm.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() + retry_in, PendingIntent.getService(this, 0, new Intent(this, DoNothingService.class), 0));
+                }
+            } else {
+                stopSelf();
+            }
     }
 
     public void listenForChangeInSettings() {
