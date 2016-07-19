36746b9c2ebd3daa073ebf2d21748f1787eaa5b4
Timm Korte <korte-github@easycrypt.de>
2016-07-19 23:58:10 +0200

    undo battery fix to rebase to upstream/master

--- app/src/main/java/com/eveningoutpost/dexdrip/Home.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Home.java
@@ -1551,6 +1551,8 @@ public class Home extends ActivityWithMenu {
 
 
             }
+        } else {
+            dexbridgeBattery.setVisibility(View.INVISIBLE);
         }
         if (!prefs.getBoolean("display_bridge_battery", true)) {
             dexbridgeBattery.setVisibility(View.INVISIBLE);
