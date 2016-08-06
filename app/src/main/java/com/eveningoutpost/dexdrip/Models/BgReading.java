dd8e5ffc53d8051a2fd43a8064f0ad09f5c86020
Tzachi Dar <tzachi.dar@gmail.com>
2016-08-07 01:25:23 +0300

    Take the correct time stamp even when find_new_raw_curve is not called on last packet.

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/BgReading.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/BgReading.java
@@ -990,11 +990,13 @@ public class BgReading extends Model implements ShareUploadableBg {
     public void find_new_raw_curve() {
         List<BgReading> last_3 = BgReading.latest(3);
         if ((last_3 != null) && (last_3.size() == 3)) {
-            BgReading second_latest = last_3.get(1);
-            BgReading third_latest = last_3.get(2);
 
-            double y3 = age_adjusted_raw_value;
-            double x3 = timestamp;
+            final BgReading latest = last_3.get(0);
+            final BgReading second_latest = last_3.get(1);
+            final BgReading third_latest = last_3.get(2);
+
+            double y3 = latest.age_adjusted_raw_value;
+            double x3 = latest.timestamp;
             double y2 = second_latest.age_adjusted_raw_value;
             double x2 = second_latest.timestamp;
             double y1 = third_latest.age_adjusted_raw_value;
@@ -1011,7 +1013,7 @@ public class BgReading extends Model implements ShareUploadableBg {
             BgReading second_latest = last_3.get(1);
 
             double y2 = latest.age_adjusted_raw_value;
-            double x2 = timestamp;
+            double x2 = latest.timestamp;
             double y1 = second_latest.age_adjusted_raw_value;
             double x1 = second_latest.timestamp;
             if(y1 == y2) {
