50c6fb8c49ace3726dcc34160d52b4876601f569
Jamorham <jamorham@users.noreply.github.com>
2016-08-10 19:10:36 +0100

    Update calibration graph activity

--- app/src/main/java/com/eveningoutpost/dexdrip/CalibrationGraph.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/CalibrationGraph.java
@@ -1,6 +1,8 @@
 package com.eveningoutpost.dexdrip;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.widget.TextView;
 
 import com.eveningoutpost.dexdrip.Models.Calibration;
@@ -22,10 +24,10 @@ import lecho.lib.hellocharts.view.LineChartView;
 
 public class CalibrationGraph extends ActivityWithMenu {
     //public static String menu_name = "Calibration Graph";
-   private LineChartView chart;
+    private LineChartView chart;
     private LineChartData data;
-    public double  start_x = 50;
-    public double  end_x = 300;
+    public double start_x = 50;
+    public double end_x = 300;
 
     TextView GraphHeader;
 
@@ -42,58 +44,74 @@ public class CalibrationGraph extends ActivityWithMenu {
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         setupCharts();
     }
 
     public void setupCharts() {
         chart = (LineChartView) findViewById(R.id.chart);
-        List<Calibration> calibrations = Calibration.allForSensor();
-        List<PointValue> values = new ArrayList<PointValue>();
-        for (Calibration calibration : calibrations) {
-            PointValue point = new PointValue((float)calibration.estimate_raw_at_time_of_calibration, (float)calibration.bg);
-            String time = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(new Date((long)calibration.raw_timestamp));
-            point.setLabel(time.toCharArray());
-            values.add(point);
-        }
+        List<Line> lines = new ArrayList<Line>();
 
-        Line line = new Line(values);
-        line.setColor(ChartUtils.COLOR_BLUE);
-        line.setHasLines(false);
-        line.setPointRadius(4);
-        line.setHasPoints(true);
-        line.setHasLabels(true);
-
-        Calibration calibration = Calibration.last();
-        List<PointValue> lineValues = new ArrayList<PointValue>();
-        if(calibration != null) {
-            lineValues.add(new PointValue((float) start_x, (float) (start_x * calibration.slope + calibration.intercept)));
-            lineValues.add(new PointValue((float) end_x, (float) (end_x * calibration.slope + calibration.intercept)));
-
+        Calibration calibration = Calibration.lastValid();
+        if (calibration != null) {
+            //set header
             DecimalFormat df = new DecimalFormat("#");
             df.setMaximumFractionDigits(2);
             df.setMinimumFractionDigits(2);
             String Header = "slope = " + df.format(calibration.slope) + " intercept = " + df.format(calibration.intercept);
             GraphHeader.setText(Header);
+
+            //red line
+            List<PointValue> lineValues = new ArrayList<PointValue>();
+            lineValues.add(new PointValue((float) start_x, (float) (start_x * calibration.slope + calibration.intercept)));
+            lineValues.add(new PointValue((float) end_x, (float) (end_x * calibration.slope + calibration.intercept)));
+            Line calibrationLine = new Line(lineValues);
+            calibrationLine.setColor(ChartUtils.COLOR_RED);
+            calibrationLine.setHasLines(true);
+            calibrationLine.setHasPoints(false);
+
+            //calibration values
+            List<Calibration> calibrations = Calibration.allForSensor();
+            Line greyLine = getCalibrationsLine(calibrations, Color.parseColor("#66FFFFFF"));
+            calibrations = Calibration.allForSensorInLastFourDays();
+            Line blueLine = getCalibrationsLine(calibrations, ChartUtils.COLOR_BLUE);
+
+            //add lines in order
+            lines.add(greyLine);
+            lines.add(blueLine);
+            lines.add(calibrationLine);
+
         }
-        Line calibrationLine = new Line(lineValues);
-        calibrationLine.setColor(ChartUtils.COLOR_RED);
-        calibrationLine.setHasLines(true);
-        calibrationLine.setHasPoints(false);
         Axis axisX = new Axis();
         Axis axisY = new Axis().setHasLines(true);
         axisX.setName("Raw Value");
         axisY.setName("BG");
 
-         List<Line> lines = new ArrayList<Line>();
-        lines.add(line);
-        lines.add(calibrationLine);
 
         data = new LineChartData(lines);
         data.setAxisXBottom(axisX);
         data.setAxisYLeft(axisY);
         chart.setLineChartData(data);
 
+    }
+
+    @NonNull
+    public Line getCalibrationsLine(List<Calibration> calibrations, int color) {
+        List<PointValue> values = new ArrayList<PointValue>();
+        for (Calibration calibration : calibrations) {
+            PointValue point = new PointValue((float) calibration.estimate_raw_at_time_of_calibration, (float) calibration.bg);
+            String time = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(new Date((long) calibration.raw_timestamp));
+            point.setLabel(time.toCharArray());
+            values.add(point);
+        }
+
+        Line line = new Line(values);
+        line.setColor(color);
+        line.setHasLines(false);
+        line.setPointRadius(4);
+        line.setHasPoints(true);
+        line.setHasLabels(true);
+        return line;
     }
 }
