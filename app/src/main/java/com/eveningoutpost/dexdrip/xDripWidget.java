ace5f63dce27133a75510c40443ac64b991ac1eb
Jamorham <jamorham@users.noreply.github.com>
2016-09-11 18:38:22 +0100

    Widget fix

--- app/src/main/java/com/eveningoutpost/dexdrip/xDripWidget.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/xDripWidget.java
@@ -78,97 +78,100 @@ public class xDripWidget extends AppWidgetProvider {
         if (lastBgreading != null) {
             double estimate = 0;
             double estimated_delta = -9999;
-            int height = appWidgetManager.getAppWidgetOptions(appWidgetId).getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
-            int width = appWidgetManager.getAppWidgetOptions(appWidgetId).getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
-            views.setImageViewBitmap(R.id.widgetGraph, new BgSparklineBuilder(context)
-                    .setBgGraphBuilder(bgGraphBuilder)
-                            //.setShowFiltered(Home.getPreferencesBooleanDefaultFalse("show_filtered_curve"))
-                    .setBackgroundColor(ColorCache.getCol(ColorCache.X.color_widget_chart_background))
-                    .setHeight(height).setWidth(width).build());
+            try {
+                int height = appWidgetManager.getAppWidgetOptions(appWidgetId).getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+                int width = appWidgetManager.getAppWidgetOptions(appWidgetId).getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+                views.setImageViewBitmap(R.id.widgetGraph, new BgSparklineBuilder(context)
+                        .setBgGraphBuilder(bgGraphBuilder)
+                        //.setShowFiltered(Home.getPreferencesBooleanDefaultFalse("show_filtered_curve"))
+                        .setBackgroundColor(ColorCache.getCol(ColorCache.X.color_widget_chart_background))
+                        .setHeight(height).setWidth(width).build());
 
-            estimate = lastBgreading.calculated_value;
-            String extrastring = "";
-            String slope_arrow = lastBgreading.slopeArrow();
-            String stringEstimate;
+                estimate = lastBgreading.calculated_value;
+                String extrastring = "";
+                String slope_arrow = lastBgreading.slopeArrow();
+                String stringEstimate;
 
-            if ((BgGraphBuilder.last_noise > BgGraphBuilder.NOISE_TRIGGER)
-                    && (BgGraphBuilder.best_bg_estimate > 0)
-                    && (BgGraphBuilder.last_bg_estimate > 0)
-                    && (PreferenceManager.getDefaultSharedPreferences(context).getBoolean("bg_compensate_noise", false))) {
-                estimate = BgGraphBuilder.best_bg_estimate; // this needs scaling based on noise intensity
-                estimated_delta = BgGraphBuilder.best_bg_estimate - BgGraphBuilder.last_bg_estimate;
-                slope_arrow = BgReading.slopeToArrowSymbol(estimated_delta / (BgGraphBuilder.DEXCOM_PERIOD / 60000)); // delta by minute
-                //currentBgValueText.setTypeface(null, Typeface.ITALIC);
-                extrastring = " \u26A0 "; // warning symbol !
+                if ((BgGraphBuilder.last_noise > BgGraphBuilder.NOISE_TRIGGER)
+                        && (BgGraphBuilder.best_bg_estimate > 0)
+                        && (BgGraphBuilder.last_bg_estimate > 0)
+                        && (PreferenceManager.getDefaultSharedPreferences(context).getBoolean("bg_compensate_noise", false))) {
+                    estimate = BgGraphBuilder.best_bg_estimate; // this needs scaling based on noise intensity
+                    estimated_delta = BgGraphBuilder.best_bg_estimate - BgGraphBuilder.last_bg_estimate;
+                    slope_arrow = BgReading.slopeToArrowSymbol(estimated_delta / (BgGraphBuilder.DEXCOM_PERIOD / 60000)); // delta by minute
+                    //currentBgValueText.setTypeface(null, Typeface.ITALIC);
+                    extrastring = " \u26A0 "; // warning symbol !
 
-            }
+                }
 
 
-
-            if ((new Date().getTime()) - Home.stale_data_millis() - lastBgreading.timestamp > 0) {
+                if ((new Date().getTime()) - Home.stale_data_millis() - lastBgreading.timestamp > 0) {
 //                estimate = lastBgreading.calculated_value;
-                Log.d(TAG, "old value, estimate " + estimate);
-                 stringEstimate = bgGraphBuilder.unitized_string(estimate);
+                    Log.d(TAG, "old value, estimate " + estimate);
+                    stringEstimate = bgGraphBuilder.unitized_string(estimate);
 
-                //views.setTextViewText(R.id.widgetArrow, "--");
-                slope_arrow = "--";
-                views.setInt(R.id.widgetBg, "setPaintFlags", Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
-            } else {
-//                estimate = lastBgreading.calculated_value;
-                 stringEstimate = bgGraphBuilder.unitized_string(estimate);
-
-                if (lastBgreading.hide_slope) {
+                    //views.setTextViewText(R.id.widgetArrow, "--");
                     slope_arrow = "--";
-                }
-                Log.d(TAG, "newish value, estimate " + stringEstimate + slope_arrow);
-
-
-                views.setInt(R.id.widgetBg, "setPaintFlags", 0);
-            }
-            views.setTextViewText(R.id.widgetBg, stringEstimate);
-            views.setTextViewText(R.id.widgetArrow, slope_arrow);
-
-            // is it really necessary to read this data once here and again in unitizedDeltaString?
-            // couldn't we just use the unitizedDeltaString to detect the error condition?
-            List<BgReading> bgReadingList =  BgReading.latest(2,Home.get_follower());
-
-            if (estimated_delta == -9999) {
-                // use original delta
-                if (bgReadingList != null && bgReadingList.size() == 2) {
-
-                    views.setTextViewText(R.id.widgetDelta, bgGraphBuilder.unitizedDeltaString(true, true, Home.get_follower()));
+                    views.setInt(R.id.widgetBg, "setPaintFlags", Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
                 } else {
-                    views.setTextViewText(R.id.widgetDelta, "--");
+//                estimate = lastBgreading.calculated_value;
+                    stringEstimate = bgGraphBuilder.unitized_string(estimate);
+
+                    if (lastBgreading.hide_slope) {
+                        slope_arrow = "--";
+                    }
+                    Log.d(TAG, "newish value, estimate " + stringEstimate + slope_arrow);
+
+
+                    views.setInt(R.id.widgetBg, "setPaintFlags", 0);
                 }
-            } else {
-                // use compensated estimate
-                views.setTextViewText(R.id.widgetDelta, bgGraphBuilder.unitizedDeltaStringRaw(true, true, estimated_delta));
-            }
+                views.setTextViewText(R.id.widgetBg, stringEstimate);
+                views.setTextViewText(R.id.widgetArrow, slope_arrow);
 
-            int timeAgo =(int) Math.floor((new Date().getTime() - lastBgreading.timestamp)/(1000*60));
-            if (timeAgo == 1) {
-                views.setTextViewText(R.id.readingAge, timeAgo + " Minute ago"+extrastring);
-            } else {
-                views.setTextViewText(R.id.readingAge, timeAgo + " Minutes ago"+extrastring);
-            }
-            if (timeAgo > 15) {
-                views.setTextColor(R.id.readingAge, Color.parseColor("#FFBB33"));
-            } else {
-                views.setTextColor(R.id.readingAge, Color.WHITE);
-            }
+                // is it really necessary to read this data once here and again in unitizedDeltaString?
+                // couldn't we just use the unitizedDeltaString to detect the error condition?
+                List<BgReading> bgReadingList = BgReading.latest(2, Home.get_follower());
 
-            if (bgGraphBuilder.unitized(estimate) <= bgGraphBuilder.lowMark) {
-                views.setTextColor(R.id.widgetBg, Color.parseColor("#C30909"));
-                views.setTextColor(R.id.widgetDelta, Color.parseColor("#C30909"));
-                views.setTextColor(R.id.widgetArrow, Color.parseColor("#C30909"));
-            } else if (bgGraphBuilder.unitized(estimate) >= bgGraphBuilder.highMark) {
-                views.setTextColor(R.id.widgetBg, Color.parseColor("#FFBB33"));
-                views.setTextColor(R.id.widgetDelta, Color.parseColor("#FFBB33"));
-                views.setTextColor(R.id.widgetArrow, Color.parseColor("#FFBB33"));
-            } else {
-                views.setTextColor(R.id.widgetBg, Color.WHITE);
-                views.setTextColor(R.id.widgetDelta, Color.WHITE);
-                views.setTextColor(R.id.widgetArrow, Color.WHITE);
+                if (estimated_delta == -9999) {
+                    // use original delta
+                    if (bgReadingList != null && bgReadingList.size() == 2) {
+
+                        views.setTextViewText(R.id.widgetDelta, bgGraphBuilder.unitizedDeltaString(true, true, Home.get_follower()));
+                    } else {
+                        views.setTextViewText(R.id.widgetDelta, "--");
+                    }
+                } else {
+                    // use compensated estimate
+                    views.setTextViewText(R.id.widgetDelta, bgGraphBuilder.unitizedDeltaStringRaw(true, true, estimated_delta));
+                }
+
+                int timeAgo = (int) Math.floor((new Date().getTime() - lastBgreading.timestamp) / (1000 * 60));
+                if (timeAgo == 1) {
+                    views.setTextViewText(R.id.readingAge, timeAgo + " Minute ago" + extrastring);
+                } else {
+                    views.setTextViewText(R.id.readingAge, timeAgo + " Minutes ago" + extrastring);
+                }
+                if (timeAgo > 15) {
+                    views.setTextColor(R.id.readingAge, Color.parseColor("#FFBB33"));
+                } else {
+                    views.setTextColor(R.id.readingAge, Color.WHITE);
+                }
+
+                if (bgGraphBuilder.unitized(estimate) <= bgGraphBuilder.lowMark) {
+                    views.setTextColor(R.id.widgetBg, Color.parseColor("#C30909"));
+                    views.setTextColor(R.id.widgetDelta, Color.parseColor("#C30909"));
+                    views.setTextColor(R.id.widgetArrow, Color.parseColor("#C30909"));
+                } else if (bgGraphBuilder.unitized(estimate) >= bgGraphBuilder.highMark) {
+                    views.setTextColor(R.id.widgetBg, Color.parseColor("#FFBB33"));
+                    views.setTextColor(R.id.widgetDelta, Color.parseColor("#FFBB33"));
+                    views.setTextColor(R.id.widgetArrow, Color.parseColor("#FFBB33"));
+                } else {
+                    views.setTextColor(R.id.widgetBg, Color.WHITE);
+                    views.setTextColor(R.id.widgetDelta, Color.WHITE);
+                    views.setTextColor(R.id.widgetArrow, Color.WHITE);
+                }
+            } catch (RuntimeException e) {
+                Log.e(TAG, "Got exception in displaycurrentinfo: " + e);
             }
         }
     }
