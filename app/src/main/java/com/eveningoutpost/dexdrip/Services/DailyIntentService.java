76556f88f010bb7781921c8a11b3fcad773bbdcf
Tzachi Dar <tzachi.dar@gmail.com>
2016-11-20 01:13:35 +0200

    Add logging for Daily intent service.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- app/src/main/java/com/eveningoutpost/dexdrip/Services/DailyIntentService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Services/DailyIntentService.java
@@ -11,10 +11,12 @@ import com.eveningoutpost.dexdrip.Models.UserError;
 import com.eveningoutpost.dexdrip.UtilityModels.BgSendQueue;
 import com.eveningoutpost.dexdrip.UtilityModels.CalibrationSendQueue;
 import com.eveningoutpost.dexdrip.UtilityModels.UploaderQueue;
+import com.eveningoutpost.dexdrip.Models.UserError.Log;
 
 import static com.eveningoutpost.dexdrip.UtilityModels.UpdateActivity.checkForAnUpdate;
 
 public class DailyIntentService extends IntentService {
+    private final static String TAG = DailyIntentService.class.getSimpleName();
     // DAILY TASKS CAN GO IN HERE!
 
     public DailyIntentService() {
@@ -24,39 +26,42 @@ public class DailyIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         final PowerManager.WakeLock wl = JoH.getWakeLock("DailyIntentService", 120000);
-
+        Log.i(TAG, "DailyIntentService onHandleIntent Starting");
+        Long start = JoH.tsl();
         // prune old database records
         try {
             UserError.cleanup();
         } catch (Exception e) {
-            //
+            Log.e(TAG, "DailyIntentService exception on UserError ", e);
         }
         try {
             BgSendQueue.cleanQueue();
+            
         } catch (Exception e) {
-            //
+            Log.e(TAG, "DailyIntentService exception on BgSendQueue ", e);
         }
         try {
             CalibrationSendQueue.cleanQueue();
         } catch (Exception e) {
-            //
+            Log.e(TAG, "DailyIntentService exception on CalibrationSendQueue ", e);
         }
         try {
             UploaderQueue.cleanQueue();
         } catch (Exception e) {
-            //
+            Log.e(TAG, "DailyIntentService exception on UploaderQueue ", e);
         }
         try {
             PebbleMovement.cleanup(Home.getPreferencesInt("retention_pebble_movement", 180));
         } catch (Exception e) {
-            //
+            Log.e(TAG, "DailyIntentService exception on PebbleMovement ", e);
         }
         try {
             checkForAnUpdate(getApplicationContext());
         } catch (Exception e) {
-            //
+            Log.e(TAG, "DailyIntentService exception on checkForAnUpdate ", e);
         }
         JoH.releaseWakeLock(wl);
+        Log.i(TAG, "DailyIntentService onHandleIntent exiting after " + ((JoH.tsl() - start) / 1000) + " seconds");
     }
 
 }
