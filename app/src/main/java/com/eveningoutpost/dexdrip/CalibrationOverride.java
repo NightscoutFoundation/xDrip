9685650d9cde051197b4b487612dc03e80290559
Jamorham <jamorham@users.noreply.github.com>
2016-12-06 13:15:08 +0000

    Calibration Override number format fix

--- app/src/main/java/com/eveningoutpost/dexdrip/CalibrationOverride.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/CalibrationOverride.java
@@ -45,39 +45,43 @@ public class CalibrationOverride extends ActivityWithMenu {
     public void addListenerOnButton() {
             button = (Button) findViewById(R.id.save_calibration_button);
 
-            button.setOnClickListener(new View.OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (Sensor.isActive()) {
                     EditText value = (EditText) findViewById(R.id.bg_value);
                     String string_value = value.getText().toString();
-                    if (!TextUtils.isEmpty(string_value)){
-                        final double calValue = JoH.tolerantParseDouble(string_value);
+                    if (!TextUtils.isEmpty(string_value)) {
+                        try {
+                            final double calValue = JoH.tolerantParseDouble(string_value);
 
-                        Calibration last_calibration = Calibration.lastValid();
-                        last_calibration.sensor_confidence = 0;
-                        last_calibration.slope_confidence = 0;
-                        last_calibration.save();
-                        CalibrationSendQueue.addToQueue(last_calibration, getApplicationContext());
-                        // TODO we need to push the nixing of this last calibration
+                            Calibration last_calibration = Calibration.lastValid();
+                            last_calibration.sensor_confidence = 0;
+                            last_calibration.slope_confidence = 0;
+                            last_calibration.save();
+                            CalibrationSendQueue.addToQueue(last_calibration, getApplicationContext());
+                            // TODO we need to push the nixing of this last calibration
 
-                        final Calibration calibration = Calibration.create(calValue, getApplicationContext());
-                        if (calibration != null) {
-                            UndoRedo.addUndoCalibration(calibration.uuid);
-                            GcmActivity.pushCalibration(string_value, "0");
+                            final Calibration calibration = Calibration.create(calValue, getApplicationContext());
+                            if (calibration != null) {
+                                UndoRedo.addUndoCalibration(calibration.uuid);
+                                GcmActivity.pushCalibration(string_value, "0");
 
-                            final boolean wear_integration = Home.getPreferencesBoolean("wear_sync", false);//KS
-                            if (wear_integration) {
-                                android.util.Log.d("CalibrationOverride", "start WatchUpdaterService with ACTION_SYNC_CALIBRATION");
-                                startService(new Intent(v.getContext(), WatchUpdaterService.class).setAction(WatchUpdaterService.ACTION_SYNC_CALIBRATION));
+                                final boolean wear_integration = Home.getPreferencesBoolean("wear_sync", false);//KS
+                                if (wear_integration) {
+                                    android.util.Log.d("CalibrationOverride", "start WatchUpdaterService with ACTION_SYNC_CALIBRATION");
+                                    startService(new Intent(v.getContext(), WatchUpdaterService.class).setAction(WatchUpdaterService.ACTION_SYNC_CALIBRATION));
+                                }
+
+                            } else {
+                                Log.e(TAG, "Calibration creation resulted in null");
+                                JoH.static_toast_long("Could not create calibration!");
                             }
-
-                        } else {
-                            Log.e(TAG, "Calibration creation resulted in null");
-                            JoH.static_toast_long("Could not create calibration!");
+                            Intent tableIntent = new Intent(v.getContext(), Home.class);
+                            startActivity(tableIntent);
+                            finish();
+                        } catch (NumberFormatException e) {
+                            value.setError("Number error: " + e);
                         }
-                         Intent tableIntent = new Intent(v.getContext(), Home.class);
-                         startActivity(tableIntent);
-                         finish();
                     } else {
                         value.setError("Calibration Can Not be blank");
                     }
