9d0d54daf51537ea23c6e16cc021135ab11cd201
Jamorham <jamorham@users.noreply.github.com>
2016-12-15 11:20:02 +0000

    DailyIntentService: add rate limiter

--- app/src/main/java/com/eveningoutpost/dexdrip/Services/DailyIntentService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Services/DailyIntentService.java
@@ -8,10 +8,10 @@ import com.eveningoutpost.dexdrip.Home;
 import com.eveningoutpost.dexdrip.Models.JoH;
 import com.eveningoutpost.dexdrip.Models.PebbleMovement;
 import com.eveningoutpost.dexdrip.Models.UserError;
+import com.eveningoutpost.dexdrip.Models.UserError.Log;
 import com.eveningoutpost.dexdrip.UtilityModels.BgSendQueue;
 import com.eveningoutpost.dexdrip.UtilityModels.CalibrationSendQueue;
 import com.eveningoutpost.dexdrip.UtilityModels.UploaderQueue;
-import com.eveningoutpost.dexdrip.Models.UserError.Log;
 
 import static com.eveningoutpost.dexdrip.UtilityModels.UpdateActivity.checkForAnUpdate;
 
@@ -26,42 +26,46 @@ public class DailyIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         final PowerManager.WakeLock wl = JoH.getWakeLock("DailyIntentService", 120000);
-        Log.i(TAG, "DailyIntentService onHandleIntent Starting");
-        Long start = JoH.tsl();
-        // prune old database records
-        try {
-            UserError.cleanup();
-        } catch (Exception e) {
-            Log.e(TAG, "DailyIntentService exception on UserError ", e);
-        }
-        try {
-            BgSendQueue.cleanQueue();
-            
-        } catch (Exception e) {
-            Log.e(TAG, "DailyIntentService exception on BgSendQueue ", e);
-        }
-        try {
-            CalibrationSendQueue.cleanQueue();
-        } catch (Exception e) {
-            Log.e(TAG, "DailyIntentService exception on CalibrationSendQueue ", e);
-        }
-        try {
-            UploaderQueue.cleanQueue();
-        } catch (Exception e) {
-            Log.e(TAG, "DailyIntentService exception on UploaderQueue ", e);
-        }
-        try {
-            PebbleMovement.cleanup(Home.getPreferencesInt("retention_pebble_movement", 180));
-        } catch (Exception e) {
-            Log.e(TAG, "DailyIntentService exception on PebbleMovement ", e);
-        }
-        try {
-            checkForAnUpdate(getApplicationContext());
-        } catch (Exception e) {
-            Log.e(TAG, "DailyIntentService exception on checkForAnUpdate ", e);
+        if (JoH.pratelimit("daily-intent-service", 60000)) {
+            Log.i(TAG, "DailyIntentService onHandleIntent Starting");
+            Long start = JoH.tsl();
+            // prune old database records
+            try {
+                UserError.cleanup();
+            } catch (Exception e) {
+                Log.e(TAG, "DailyIntentService exception on UserError ", e);
+            }
+            try {
+                BgSendQueue.cleanQueue();
+
+            } catch (Exception e) {
+                Log.e(TAG, "DailyIntentService exception on BgSendQueue ", e);
+            }
+            try {
+                CalibrationSendQueue.cleanQueue();
+            } catch (Exception e) {
+                Log.e(TAG, "DailyIntentService exception on CalibrationSendQueue ", e);
+            }
+            try {
+                UploaderQueue.cleanQueue();
+            } catch (Exception e) {
+                Log.e(TAG, "DailyIntentService exception on UploaderQueue ", e);
+            }
+            try {
+                PebbleMovement.cleanup(Home.getPreferencesInt("retention_pebble_movement", 180));
+            } catch (Exception e) {
+                Log.e(TAG, "DailyIntentService exception on PebbleMovement ", e);
+            }
+            try {
+                checkForAnUpdate(getApplicationContext());
+            } catch (Exception e) {
+                Log.e(TAG, "DailyIntentService exception on checkForAnUpdate ", e);
+            }
+            Log.i(TAG, "DailyIntentService onHandleIntent exiting after " + ((JoH.tsl() - start) / 1000) + " seconds");
+        } else {
+            Log.e(TAG, "DailyIntentService exceeding rate limit");
         }
         JoH.releaseWakeLock(wl);
-        Log.i(TAG, "DailyIntentService onHandleIntent exiting after " + ((JoH.tsl() - start) / 1000) + " seconds");
     }
 
 }
