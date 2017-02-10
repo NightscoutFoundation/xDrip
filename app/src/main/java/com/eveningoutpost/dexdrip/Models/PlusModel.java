ea71722889947bf4b3a6daa1dffebc99375cb853
Jamorham <jamorham@users.noreply.github.com>
2017-02-10 20:24:24 +0000

    Plus Model: initial commit

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/PlusModel.java
@@ -0,0 +1,26 @@
+package com.eveningoutpost.dexdrip.Models;
+
+import com.activeandroid.Model;
+import com.activeandroid.util.SQLiteUtils;
+
+/**
+ * Created by jamorham on 01/02/2017.
+ */
+
+public class PlusModel extends Model {
+
+    private static boolean patched = false;
+
+    private synchronized static void fixUpTable(String[] schema) {
+        if (patched) return;
+
+        for (String patch : schema) {
+            try {
+                SQLiteUtils.execSql(patch);
+            } catch (Exception e) {
+            }
+        }
+        patched = true;
+    }
+
+}
