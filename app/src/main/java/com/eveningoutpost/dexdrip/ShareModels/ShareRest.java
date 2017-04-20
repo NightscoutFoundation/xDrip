0fe2a0235ab31b5d489e163d1196fdb4715d9870
Jamorham <jamorham@users.noreply.github.com>
2017-04-20 13:06:17 +0100

    ShareRest: finalize instance

--- app/src/main/java/com/eveningoutpost/dexdrip/ShareModels/ShareRest.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/ShareModels/ShareRest.java
@@ -128,7 +128,7 @@ public class ShareRest {
             sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
             final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
-            OkHttpClient okHttpClient = new OkHttpClient();
+            final OkHttpClient okHttpClient = new OkHttpClient();
             okHttpClient.networkInterceptors().add(new Interceptor() {
                 @Override
                 public Response intercept(Chain chain) throws IOException {
