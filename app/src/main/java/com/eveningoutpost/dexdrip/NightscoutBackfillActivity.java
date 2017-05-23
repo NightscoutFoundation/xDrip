40166d051e7b4b981a639f9abe8d605891bca428
Jamorham <jamorham@users.noreply.github.com>
2017-05-23 22:38:00 +0100

    Nightscout: Add back-filling option in rest-api extra options

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/NightscoutBackfillActivity.java
@@ -0,0 +1,139 @@
+package com.eveningoutpost.dexdrip;
+
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.eveningoutpost.dexdrip.Models.BgReading;
+import com.eveningoutpost.dexdrip.Models.JoH;
+import com.eveningoutpost.dexdrip.Models.Treatments;
+import com.eveningoutpost.dexdrip.Services.SyncService;
+import com.eveningoutpost.dexdrip.UtilityModels.Constants;
+import com.eveningoutpost.dexdrip.UtilityModels.MongoSendTask;
+import com.eveningoutpost.dexdrip.UtilityModels.PersistentStore;
+import com.eveningoutpost.dexdrip.UtilityModels.UploaderQueue;
+import com.eveningoutpost.dexdrip.profileeditor.DatePickerFragment;
+import com.eveningoutpost.dexdrip.profileeditor.ProfileAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by jamorham on 23/05/2017.
+ * <p>
+ * Manage and kick off a backfill request
+ */
+
+public class NightscoutBackfillActivity extends AppCompatActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private final static String TAG = "BackFillActivity";
+    private Button dateButton;
+    private Button doitButton;
+    private Calendar calendar;
+
+    private static long locked = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nightscout_backfill);
+        JoH.fixActionBar(this);
+        dateButton = (Button) findViewById(R.id.backfillDateButton);
+        doitButton = (Button) findViewById(R.id.startbackfill);
+        calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(JoH.tsl() - Constants.DAY_IN_MS);
+        updateDateButton();
+    }
+
+    private void updateDateButton() {
+        SimpleDateFormat format = new SimpleDateFormat("MMMM d, yyyy h:mm a");
+        dateButton.setText(format.format(calendar.getTime()));
+    }
+
+    @Override
+    protected void onResume() {
+        xdrip.checkForcedEnglish(this);
+        super.onResume();
+        mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), "Nightscout Backfill", this);
+
+        if (JoH.msSince(locked) < Constants.HOUR_IN_MS) {
+            JoH.static_toast_long("Still processing previous back-fill request!");
+            finish();
+        }
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        mNavigationDrawerFragment.swapContext(position);
+    }
+
+    public void backfillCancel(View v) {
+        finish();
+    }
+
+    public void backfillPick(View v) {
+        final DatePickerFragment datePickerFragment = new DatePickerFragment();
+
+        datePickerFragment.setAllowFuture(false);
+        datePickerFragment.setInitiallySelectedDate(calendar.getTimeInMillis());
+        datePickerFragment.setTitle("How far back?");
+        datePickerFragment.setDateCallback(new ProfileAdapter.DatePickerCallbacks() {
+            @Override
+            public void onDateSet(int year, int month, int day) {
+                calendar.set(year, month, day);
+                updateDateButton();
+            }
+        });
+        datePickerFragment.show(this.getFragmentManager(), "DatePicker");
+    }
+
+    public synchronized void backfillRun(View v) {
+        locked = JoH.tsl();
+        doitButton.setVisibility(View.INVISIBLE);
+        JoH.static_toast_long("Please wait..");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final PowerManager.WakeLock wl = JoH.getWakeLock("nightscout-backfill", 600000);
+                try {
+                    final List<BgReading> the_readings = BgReading.latestForGraphAsc(500000, calendar.getTimeInMillis(), JoH.tsl());
+                    if ((the_readings != null) && (the_readings.size() > 0)) {
+                        PersistentStore.setBoolean(MongoSendTask.BACKFILLING_BOOSTER, true);
+                        long bgcount = the_readings.size();
+                        long trcount = 0;
+                        for (BgReading bg : the_readings) {
+                            UploaderQueue.newEntry("update", bg);
+                        }
+
+                        final List<Treatments> the_treatments = Treatments.latestForGraph(50000, calendar.getTimeInMillis(), JoH.tsl());
+                        if ((the_treatments != null) && (the_treatments.size() > 0)) {
+                            trcount = the_treatments.size();
+                            for (Treatments tr : the_treatments) {
+                                UploaderQueue.newEntry("update", tr);
+                            }
+                        }
+
+                        // TODO Calibrations? Blood tests?
+
+                        JoH.static_toast_long("Queued " + bgcount + " glucose readings and " + trcount + " treatments!");
+                        SyncService.startSyncService(500);
+                        locked = 0; // clear lock
+                    } else {
+                        JoH.static_toast_long("Didn't find any glucose readings in that time period");
+                    }
+                } finally {
+                    JoH.releaseWakeLock(wl);
+                }
+            }
+        }).start();
+
+        finish();
+    }
+
+}
