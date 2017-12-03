053559521eb84d21058bca938f29cfb845b7c0f1
Tzachi Dar <tzachi.dar@gmail.com>
2017-12-03 02:09:55 +0200

    If all alerts are disabled, do not alert for low bridge battery.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/CheckBridgeBattery.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/CheckBridgeBattery.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import com.eveningoutpost.dexdrip.Home;
 import com.eveningoutpost.dexdrip.Models.JoH;
 import com.eveningoutpost.dexdrip.Models.UserError;
+import com.eveningoutpost.dexdrip.Models.UserError.Log;
 import com.eveningoutpost.dexdrip.UtilityModels.NotificationChannels;
 import com.eveningoutpost.dexdrip.xdrip;
 
@@ -48,9 +49,18 @@ public class CheckBridgeBattery {
                 if (JoH.pratelimit("bridge-battery-warning", repeat_seconds)) {
                     notification_showing = true;
                     lowbattery = true;
+                    
+                    boolean sound = true;
+                    boolean vibrate = true;
+                    if(Home.getPreferencesLong("alerts_disabled_until", 0) > JoH.tsl()) {
+                        Log.d(TAG, "Not playing alert since Notifications are currently disabled!!");
+                    	sound = false;
+                    	vibrate = false;
+                    }
+                    
                     final PendingIntent pendingIntent = android.app.PendingIntent.getActivity(xdrip.getAppContext(), 0, new Intent(xdrip.getAppContext(), Home.class), android.app.PendingIntent.FLAG_UPDATE_CURRENT);
                     showNotification("Low bridge battery", "Bridge battery dropped to: " + this_level + "%",
-                            pendingIntent, NOTIFICATION_ITEM, NotificationChannels.LOW_BRIDGE_BATTERY_CHANNEL, true, true, null, null, null);
+                            pendingIntent, NOTIFICATION_ITEM, NotificationChannels.LOW_BRIDGE_BATTERY_CHANNEL, sound, vibrate, null, null, null);
                 }
             } else {
                 if (notification_showing) {
