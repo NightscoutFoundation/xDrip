0362852dcecd7a3db45bcc91c30ece881b2d5771
Tzachi Dar <tzachi.dar@gmail.com>
2017-12-29 02:05:18 +0200

    Allow librealarm to work with OOP algorithm.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- app/src/main/java/com/eveningoutpost/dexdrip/LibreAlarmReceiver.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/LibreAlarmReceiver.java
@@ -10,10 +10,13 @@ import android.os.PowerManager;
 import android.preference.PreferenceManager;
 
 import com.eveningoutpost.dexdrip.Models.UserError.Log;
+import com.eveningoutpost.dexdrip.ImportedLibraries.usbserial.util.HexDump;
 import com.eveningoutpost.dexdrip.Models.BgReading;
 import com.eveningoutpost.dexdrip.Models.Forecast;
 import com.eveningoutpost.dexdrip.Models.GlucoseData;
 import com.eveningoutpost.dexdrip.Models.JoH;
+import com.eveningoutpost.dexdrip.Models.LibreBlock;
+import com.eveningoutpost.dexdrip.Models.LibreOOPAlgorithm;
 import com.eveningoutpost.dexdrip.Models.ReadingData;
 import com.eveningoutpost.dexdrip.UtilityModels.Constants;
 import com.eveningoutpost.dexdrip.UtilityModels.Intents;
@@ -168,6 +171,15 @@ public class LibreAlarmReceiver extends BroadcastReceiver {
     }
 
     public static void processReadingDataTransferObject(ReadingData.TransferObject object) {
+    	Log.i(TAG, "Data that was recieved from librealarm is " + HexDump.dumpHexString(object.data.raw_data));
+    	// Save raw block record (we start from block 0)
+        LibreBlock.createAndSave("LibreAlarm", object.data.raw_data, 0);
+
+        if(Home.getPreferencesBooleanDefaultFalse("external_blukon_algorithm")) {
+        	LibreOOPAlgorithm.SendData(object.data.raw_data);
+        	return;
+        }
+    	
         // insert any recent data we can
         final List<GlucoseData> mTrend = object.data.trend;
         if (mTrend != null) {
