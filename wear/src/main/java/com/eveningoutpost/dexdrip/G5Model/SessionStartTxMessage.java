eb98953de44db8aebc3f642ad2f9a76dbb711638
Jamorham <jamorham@users.noreply.github.com>
2018-05-16 19:54:54 +0100

    OB1 G5: merge SessionStartTx changes

--- wear/src/main/java/com/eveningoutpost/dexdrip/G5Model/SessionStartTxMessage.java
+++ wear/src/main/java/com/eveningoutpost/dexdrip/G5Model/SessionStartTxMessage.java
@@ -23,13 +23,27 @@ public class SessionStartTxMessage extends TransmitterMessage {
     }
 
     public SessionStartTxMessage(long startTime, int dexTime) {
+        this(startTime, dexTime, null);
+    }
+
+    public SessionStartTxMessage(long startTime, int dexTime, String code) {
         this.startTime = startTime;
         this.dexTime = dexTime;
-        data = ByteBuffer.allocate(11);
+        data = ByteBuffer.allocate(code == null ? 11 : 15);
         data.order(ByteOrder.LITTLE_ENDIAN);
         data.put(opcode);
         data.putInt(dexTime);
         data.putInt((int) (startTime / 1000));
+
+        if (code != null) {
+            final G6CalibrationParameters params = new G6CalibrationParameters(code);
+            if (params.isValid()) {
+                data.putShort((short) params.getParamA());
+                data.putShort((short) params.getParamB());
+            } else {
+                throw new IllegalArgumentException("Invalid G6 code in SessionStartTxMessage");
+            }
+        }
         appendCRC();
         UserError.Log.d(TAG, "SessionStartTxMessage dbg: " + JoH.bytesToHex(byteSequence));
     }
