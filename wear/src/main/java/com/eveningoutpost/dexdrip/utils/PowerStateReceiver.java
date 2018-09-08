40b407689412d0983d5716e096bb5f0b3d6cc81b
Jamorham <jamorham@users.noreply.github.com>
2018-09-08 10:31:34 +0100

    Wear: update PowerStateReceiver

--- wear/src/main/java/com/eveningoutpost/dexdrip/utils/PowerStateReceiver.java
+++ wear/src/main/java/com/eveningoutpost/dexdrip/utils/PowerStateReceiver.java
@@ -3,7 +3,9 @@ package com.eveningoutpost.dexdrip.utils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.BatteryManager;
 import android.util.Log;
 
 import com.eveningoutpost.dexdrip.xdrip;
@@ -36,6 +38,25 @@ public class PowerStateReceiver extends BroadcastReceiver {
 
     public static boolean is_power_connected() {
         return getInternalPrefsBoolean(PREFS_POWER_STATE);
+    }
+
+    public static int getBatteryLevel() {
+        return getBatteryLevel(xdrip.getAppContext());
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public static int getBatteryLevel(Context context) {
+        final Intent batteryIntent = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        try {
+            int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            if (level == -1 || scale == -1) {
+                return 50;
+            }
+            return (int) (((float) level / (float) scale) * 100.0f);
+        } catch (NullPointerException e) {
+            return 50;
+        }
     }
 
     @Override
