ca9375e5f4e1a38d00ae80b88d2e0bf67c56e2ea
Jamorham <jamorham@users.noreply.github.com>
2018-10-08 21:52:32 +0100

    JoH: update bytesToHex

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/JoH.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/JoH.java
@@ -194,9 +194,9 @@ public class JoH {
 
     public static String bytesToHex(byte[] bytes) {
         if (bytes == null) return "<empty>";
-        char[] hexChars = new char[bytes.length * 2];
+        final char[] hexChars = new char[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
+            final int v = bytes[j] & 0xFF;
             hexChars[j * 2] = hexArray[v >>> 4];
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
