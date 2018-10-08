05389982913ba766af09561bdb71ea4f63dfc0dd
Jamorham <jamorham@users.noreply.github.com>
2018-10-08 21:56:22 +0100

    Foreground: always use foreground on oreo+

--- app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/ForegroundServiceStarter.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/UtilityModels/ForegroundServiceStarter.java
@@ -2,6 +2,7 @@ package com.eveningoutpost.dexdrip.UtilityModels;
 
 import android.app.Service;
 import android.content.Context;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 
@@ -13,32 +14,36 @@ import static com.eveningoutpost.dexdrip.UtilityModels.Notifications.ongoingNoti
  * Created by Emma Black on 12/25/14.
  */
 public class ForegroundServiceStarter {
-    private Service mService;
-    private Context mContext;
-    private boolean run_service_in_foreground;
-    private Handler mHandler;
+
+    private static final String TAG = "FOREGROUND";
+
+    final private Service mService;
+    final private Context mContext;
+    final private boolean run_service_in_foreground;
+    final private Handler mHandler;
 
 
     public ForegroundServiceStarter(Context context, Service service) {
         mContext = context;
         mService = service;
         mHandler = new Handler(Looper.getMainLooper());
-        // TODO force on with Oreo+?
-        run_service_in_foreground = Pref.getBoolean("run_service_in_foreground", false);
+        // Force foreground with Oreo and above
+        run_service_in_foreground = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O || Pref.getBoolean("run_service_in_foreground", false);
     }
 
     public void start() {
         if (mService == null) {
-            Log.e("FOREGROUND", "SERVICE IS NULL - CANNOT START!");
+            Log.e(TAG, "SERVICE IS NULL - CANNOT START!");
             return;
         }
         if (run_service_in_foreground) {
-            Log.d("FOREGROUND", "should be moving to foreground");
+            Log.d(TAG, "should be moving to foreground");
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    long end = System.currentTimeMillis() + (60000 * 5);
-                    long start = end - (60000 * 60 * 3) - (60000 * 10);
+                    // TODO use constants
+                    final long end = System.currentTimeMillis() + (60000 * 5);
+                    final long start = end - (60000 * 60 * 3) - (60000 * 10);
                     mService.startForeground(ongoingNotificationId, new Notifications().createOngoingNotification(new BgGraphBuilder(mContext, start, end), mContext));
                 }
             });
@@ -47,7 +52,7 @@ public class ForegroundServiceStarter {
 
     public void stop() {
         if (run_service_in_foreground) {
-            Log.d("FOREGROUND", "should be moving out of foreground");
+            Log.d(TAG, "should be moving out of foreground");
             mService.stopForeground(true);
         }
     }
