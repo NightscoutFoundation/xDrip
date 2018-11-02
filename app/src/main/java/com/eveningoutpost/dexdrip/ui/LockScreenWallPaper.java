57c9c29a052dc81081f4fd2dc8f08585bbc27c8e
Jamorham <jamorham@users.noreply.github.com>
2018-11-02 23:05:18 +0000

    LockScreenWallPaper: update preference listener

--- app/src/main/java/com/eveningoutpost/dexdrip/ui/LockScreenWallPaper.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/ui/LockScreenWallPaper.java
@@ -5,15 +5,21 @@ import android.app.WallpaperManager;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Build;
+import android.preference.Preference;
 
 import com.eveningoutpost.dexdrip.BestGlucose;
+import com.eveningoutpost.dexdrip.Models.JoH;
 import com.eveningoutpost.dexdrip.Models.UserError;
+import com.eveningoutpost.dexdrip.UtilityModels.Constants;
 import com.eveningoutpost.dexdrip.UtilityModels.Inevitable;
 import com.eveningoutpost.dexdrip.UtilityModels.Pref;
+import com.eveningoutpost.dexdrip.ui.activities.NumberWallPreview;
 import com.eveningoutpost.dexdrip.ui.helpers.BitmapUtil;
+import com.eveningoutpost.dexdrip.utils.time.TimeRangeUtils;
 import com.eveningoutpost.dexdrip.xdrip;
 
 import static android.app.WallpaperManager.FLAG_LOCK;
+import static com.eveningoutpost.dexdrip.ui.NumberGraphic.isLockScreenBitmapTiled;
 import static com.eveningoutpost.dexdrip.ui.helpers.BitmapUtil.getScreenHeight;
 import static com.eveningoutpost.dexdrip.ui.helpers.BitmapUtil.getScreenWidth;
 
@@ -27,24 +33,49 @@ import static com.eveningoutpost.dexdrip.ui.helpers.BitmapUtil.getScreenWidth;
 public class LockScreenWallPaper {
 
     private static final String TAG = "WallPaper";
-    private static final String PREF = "number_wall_on_lockscreen";
+    private static final String PREF_ENABLED = "number_wall_on_lockscreen";
 
+    private static final String PREF_TIME_START = "number_wall_start_time";
+    private static final String PREF_TIME_STOP = "number_wall_stop_time";
+
+    private static long lastPoll;
+
+    // check/update if not done so recently
+    public static void timerPoll() {
+        if (JoH.msSince(lastPoll) > Constants.MINUTE_IN_MS * 10) {
+            setIfEnabled();
+        }
+    }
+
+    // if feature is enabled then set() the screen
     public static void setIfEnabled() {
+        lastPoll = JoH.tsl();
         if (isEnabled()) {
             set();
         }
     }
 
-    public static void set() {
+    // set the graphic or set blank/image background if out of time period or data not good
+    private static void set() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            final BestGlucose.DisplayGlucose dg = BestGlucose.getDisplayGlucose();
-            if (dg != null && !dg.isReallyStale()) {
-                final Bitmap bitmap = NumberGraphic.getLockScreenBitmapWhite(dg.unitized, dg.delta_arrow, dg.isStale());
-                setBitmap(bitmap);
+            if (TimeRangeUtils.areWeInTimePeriod("number_wall")) {
+                final BestGlucose.DisplayGlucose dg = BestGlucose.getDisplayGlucose();
+                if (dg != null && !dg.isReallyStale()) {
+                    final Bitmap bitmap = NumberGraphic.getLockScreenBitmap(dg.unitized, dg.delta_arrow, dg.isStale());
+                    setBitmap(bitmap);
+                } else {
+                    // data too stale
+                    setBitmap(null);
+                }
             } else {
+                // not time now
                 setBitmap(null);
             }
-        }
+        } else {
+            if (JoH.ratelimit("lockscreen-wallpaper", 3600)) {
+                UserError.Log.e(TAG, "Insufficient android version to set lockscreen image");
+            }
+        } // if sufficient android version
     }
 
     public static void handlePreference() {
@@ -55,10 +86,9 @@ public class LockScreenWallPaper {
         }
     }
 
-    public static void disable() {
+    private static void disable() {
         setBitmap(null);
     }
-
 
 
     @SuppressLint("WrongConstant")
@@ -66,7 +96,9 @@ public class LockScreenWallPaper {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             try {
                 final WallpaperManager wallpaperManager = WallpaperManager.getInstance(xdrip.getAppContext());
-                wallpaperManager.setBitmap(BitmapUtil.getTiled(bitmap, getScreenWidth(), getScreenHeight()), null, false, FLAG_LOCK);
+                final Bitmap wallpaper = BitmapUtil.getTiled(bitmap, getScreenWidth(), getScreenHeight(), isLockScreenBitmapTiled(), Pref.getString(NumberWallPreview.ViewModel.PREF_numberwall_background, null));
+                wallpaperManager.setBitmap(wallpaper, null, false, FLAG_LOCK);
+                wallpaper.recycle();
             } catch (Exception e) {
                 UserError.Log.e(TAG, "Failed to set wallpaper: " + e);
             }
@@ -74,15 +106,45 @@ public class LockScreenWallPaper {
     }
 
     private static boolean isEnabled() {
-        return Pref.getBooleanDefaultFalse(PREF);
+        return Pref.getBooleanDefaultFalse(PREF_ENABLED);
     }
 
-    public static SharedPreferences.OnSharedPreferenceChangeListener prefListener = (prefs, key) -> {
-        switch (key) {
-            case PREF:
-                Inevitable.task("number-wall-handle-pref",300, LockScreenWallPaper::handlePreference);
-                break;
-        }
-    };
 
+    public static class PrefListener {
+
+        private Preference summaryPreference;
+
+        public void setSummaryPreference(Preference pref) {
+            summaryPreference = pref;
+            updateSummary();
+
+        }
+
+        public final SharedPreferences.OnSharedPreferenceChangeListener prefListener = (prefs, key) -> {
+            switch (key) {
+                case PREF_ENABLED:
+                    hardRefreshFromPreferenceChange();
+                    break;
+
+                case PREF_TIME_START:
+                case PREF_TIME_STOP:
+                    updateSummary();
+                    hardRefreshFromPreferenceChange();
+                    break;
+            }
+        };
+
+        private static void hardRefreshFromPreferenceChange() {
+            Inevitable.task("number-wall-handle-pref", 500, LockScreenWallPaper::handlePreference);
+
+        }
+
+        private void updateSummary() {
+            if (summaryPreference != null) {
+                summaryPreference.setTitle(summaryPreference.getTitle().toString().replaceAll("  \\(.+\\)$", "") + "  (" + TimeRangeUtils.getNiceStartStopString("number_wall") + ")");
+            }
+            Inevitable.task("update-number-wall", 1000, () -> setIfEnabled());
+        }
+
+    }
 }
