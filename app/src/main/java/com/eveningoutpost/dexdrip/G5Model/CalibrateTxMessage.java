e3a234e23607ccf828ed4db2b5263a4f6a968949
Jamorham <jamorham@users.noreply.github.com>
2018-11-25 13:05:17 +0000

    OB1: store glucose number in CalibrateTxMessage

--- app/src/main/java/com/eveningoutpost/dexdrip/G5Model/CalibrateTxMessage.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/G5Model/CalibrateTxMessage.java
@@ -11,8 +11,11 @@ public class CalibrateTxMessage extends BaseMessage {
     final byte opcode = 0x34;
     final int length = 9;
 
+    final int glucose;
+
     public CalibrateTxMessage(int glucose, int dexTime) {
         init(opcode, length);
+        this.glucose = glucose;
         data.putShort((short) glucose);
         data.putInt(dexTime);
         appendCRC();
