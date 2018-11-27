66fbf135f5f9704e3162ee5cdd1daff145502b86
Jamorham <jamorham@users.noreply.github.com>
2018-11-27 20:00:54 +0000

    Wear: add Root stub method

---
+++ wear/src/main/java/com/eveningoutpost/dexdrip/utils/Root.java
@@ -0,0 +1,13 @@
+package com.eveningoutpost.dexdrip.utils;
+
+// jamorham
+
+import java.util.List;
+
+public class Root {
+
+    public static List<String> exec(final String... command) {
+        // stub - not implemented on wear yet
+        return null;
+    }
+}
