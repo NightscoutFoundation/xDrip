5144871c089409f55ba9770c64c9db5fd99861b4
Jamorham <jamorham@users.noreply.github.com>
2018-11-29 14:33:36 +0000

    Tidepool: add InfoInterceptor

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/tidepool/InfoInterceptor.java
@@ -0,0 +1,31 @@
+package com.eveningoutpost.dexdrip.tidepool;
+
+import android.support.annotation.NonNull;
+
+import com.eveningoutpost.dexdrip.Models.UserError;
+
+import java.io.IOException;
+
+import lombok.RequiredArgsConstructor;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+// jamorham
+
+@RequiredArgsConstructor
+public class InfoInterceptor implements Interceptor {
+
+    private final String tag;
+
+    @Override
+    public Response intercept(@NonNull final Chain chain) throws IOException {
+        final Request request = chain.request();
+        if (request != null && request.body() != null) {
+            UserError.Log.d(tag, "Interceptor Body size: " + request.body().contentLength());
+            //} else {
+            //  UserError.Log.d(tag,"Null request body in InfoInterceptor");
+        }
+        return chain.proceed(request);
+    }
+}
