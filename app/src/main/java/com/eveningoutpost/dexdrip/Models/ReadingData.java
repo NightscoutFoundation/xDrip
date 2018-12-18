d441189d49c25c897e9c4a0e5cbc1797039a9c2e
Tzachi Dar <tzachi.dar@gmail.com>
2018-12-18 23:32:12 +0200

    Move to use 5 points in the smoothing.

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/ReadingData.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/ReadingData.java
@@ -43,9 +43,9 @@ public class ReadingData {
         }
     }
     
-    public void CalculateSmothedData() {
-        int i;
-        for (i=0; i < trend.size() - 2 ; i++) {
+    // A function to calculate the smoothing based only on 3 points.
+    private void CalculateSmothedData3Points() {
+        for (int i=0; i < trend.size() - 2 ; i++) {
             trend.get(i).glucoseLevelRawSmoothed = 
                     (trend.get(i).glucoseLevelRaw + trend.get(i+1).glucoseLevelRaw + trend.get(i+2).glucoseLevelRaw) / 3;
         }
@@ -59,8 +59,52 @@ public class ReadingData {
             // Only one point, use it
             trend.get(trend.size()-1).glucoseLevelRawSmoothed = trend.get(trend.size()-1).glucoseLevelRaw;
         }
+        
+    }
+    
+    private void CalculateSmothedData5Points() {
+        // In all places in the code, there should be exactly 16 points.
+        // Since that might change, and I'm doing an average of 5, then in the case of less then 5 points,
+        // I'll only copy the data as is (to make sure there are reasonable values when the function returns).
+        if(trend.size() < 5) {
+            for (int i=0; i < trend.size() - 4 ; i++) {
+                trend.get(i).glucoseLevelRawSmoothed = trend.get(i).glucoseLevelRaw;
+            }
+            return;
+        }
+        
+        for (int i=0; i < trend.size() - 4 ; i++) {
+            trend.get(i).glucoseLevelRawSmoothed = 
+                    (trend.get(i).glucoseLevelRaw + 
+                     trend.get(i+1).glucoseLevelRaw + 
+                     trend.get(i+2).glucoseLevelRaw + 
+                     trend.get(i+3).glucoseLevelRaw +
+                     trend.get(i+4).glucoseLevelRaw ) / 5;
+        }
+        // We now have to calculate the last 4 points, will do our best...
+        trend.get(trend.size()-4).glucoseLevelRawSmoothed = 
+                (trend.get(trend.size()-4).glucoseLevelRaw + 
+                 trend.get(trend.size()-3).glucoseLevelRaw +
+                 trend.get(trend.size()-2).glucoseLevelRaw +
+                 trend.get(trend.size()-1).glucoseLevelRaw ) / 4;
+        
+        trend.get(trend.size()-3).glucoseLevelRawSmoothed = 
+               (trend.get(trend.size()-3).glucoseLevelRaw +
+                trend.get(trend.size()-2).glucoseLevelRaw +
+                trend.get(trend.size()-1).glucoseLevelRaw ) / 3;
+
+        // Use the last two points for both last points
+        trend.get(trend.size()-2).glucoseLevelRawSmoothed = 
+                (trend.get(trend.size()-2).glucoseLevelRaw +
+                trend.get(trend.size()-1).glucoseLevelRaw ) / 2;
+        
+        trend.get(trend.size()-1).glucoseLevelRawSmoothed = trend.get(trend.size()-2).glucoseLevelRawSmoothed;
+    }
+    
+    public void CalculateSmothedData() {
+        CalculateSmothedData5Points();
         // print the values, remove before release
-        for (i=0; i < trend.size() ; i++) {
+        for (int i=0; i < trend.size() ; i++) {
             Log.e("xxx","" + i + " raw val " +  trend.get(i).glucoseLevelRaw + " smoothed " +  trend.get(i).glucoseLevelRawSmoothed);
         }
     }
