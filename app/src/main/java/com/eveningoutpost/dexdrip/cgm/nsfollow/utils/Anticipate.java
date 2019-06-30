a891c6293797b429e5b7ecec47bb975ba5d510d8
Asbjørn Aarrestad <asbjorn@aarrestad.com>
2019-06-30 21:52:25 +0200

    Updated Anticipate to be more aggressive when time is between one and two periods since last reading. This is needed for Nightscout Follow when Nightscout is in bridge mode from Dexcom Share.
    All behaviour is documented in tests.
    
    Minor changes to some neighbouring files to ease readability

--- app/src/main/java/com/eveningoutpost/dexdrip/cgm/nsfollow/utils/Anticipate.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/cgm/nsfollow/utils/Anticipate.java
@@ -1,15 +1,19 @@
 package com.eveningoutpost.dexdrip.cgm.nsfollow.utils;
 
 /**
- * jamorham
+ * Choose optimum anticipation times for re-attempting data collection to minimize
+ * number of requests to nightscout but at the same time reduce latency from new
+ * value is available till it is shown in xdrip.
  *
- * Choose optimum anticipation times for re-attempting data collection
+ * @author Original author jamorham
+ * @author Asbjørn Aarrestad - 2019.06 - asbjorn@aarrestad.com
  */
 public class Anticipate {
 
     /**
-     * If last + period and a bit< now, ask again after grace
-     * If last + period and a bit >= now, ask again after period and grace
+     * If last + period and a bit < now, ask again after grace
+     * If last + period and a bit >= now, ask again after last + period and grace
+     * if last + 2x period < now ask again after last + grace
      */
     public static long next(long now, final long last, final long period, final long grace) {
 
@@ -19,13 +23,24 @@ public class Anticipate {
             return last + period - grace;
         }
 
+        // ?: Has the period interval passed, but no reading received?
+        if (last + period < now && last + 2 * period >= now) {
+            // Yes, either missed or late reading. Try again.
+            return now + grace;
+        }
+
+        // Find time outside period schedule where we are now.
+        // Back off to save data traffic
         final long modulus = (last - now) % period;
         long nextMin;
+        // Try to wake up on next expected
         if (modulus < -grace) {
             nextMin = (now + modulus) + period - grace;
         } else {
             nextMin = (now + modulus);
         }
+
+        // Make sure result is after now.
         while (nextMin <= now) {
             nextMin += grace;
         }
--- app/src/test/java/com/eveningoutpost/dexdrip/cgm/nsfollow/utils/AnticipateTest.java
+++ app/src/test/java/com/eveningoutpost/dexdrip/cgm/nsfollow/utils/AnticipateTest.java
@@ -3,6 +3,7 @@ package com.eveningoutpost.dexdrip.cgm.nsfollow.utils;
 import org.junit.Test;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 /**
  * Tests for {@link Anticipate}
@@ -14,9 +15,9 @@ public class AnticipateTest {
     @Test
     public void next_simpleMode() {
         // :: Setup
-        long now = 10000;
-        long last = now - 1000;
-        int period = 1000;
+        long now = 10_000;
+        int period = 1_000;
+        long last = now - 1_000;
         int grace = 5;
 
         // :: Act
@@ -29,9 +30,9 @@ public class AnticipateTest {
     @Test
     public void next_lateCall() {
         // :: Setup
-        long now = 10000;
-        long last = now - 5000;
-        int period = 1000;
+        long now = 10_000;
+        long last = now - 5_000;
+        int period = 1_000;
         int grace = 5;
 
         // :: Act
@@ -42,11 +43,26 @@ public class AnticipateTest {
     }
 
     @Test
+    public void next_lateCall_overshoot() {
+        // :: Setup
+        long now = 10_000;
+        int period = 1_000;
+        long last = now - period - 200;
+        int grace = 5;
+
+        // :: Act
+        long next = Anticipate.next(now, last, period, grace);
+
+        // :: Verify
+        assertThat(next).isEqualTo(now + grace);
+    }
+
+    @Test
     public void next_waitPeriod() {
         // :: Setup
-        long now = 10000;
+        long now = 10_000;
         long last = now - 5;
-        int period = 1000;
+        int period = 1_000;
         int grace = 5;
 
         // :: Act
@@ -59,9 +75,9 @@ public class AnticipateTest {
     @Test
     public void next_waitPeriod2() {
         // :: Setup
-        long now = 10000;
+        long now = 10_000;
         long last = now - 15;
-        int period = 1000;
+        int period = 1_000;
         int grace = 5;
 
         // :: Act
@@ -69,5 +85,109 @@ public class AnticipateTest {
 
         // :: Verify
         assertThat(next).isEqualTo(last + period - grace);
+    }
+
+    // ===== Batch-verifications ===================================================================
+    @Test
+    public void next_betweenOneAndTwoPeriodsSinceLastReading() {
+        // :: Setup
+        long now = 10_000;
+        long period = 1_000;
+        int grace = 5;
+
+        int testCount = 0;
+
+        // :: Act
+        for (long last = now - period; last >= now - 2 * period; last -= (period / 2)) {
+            long next = Anticipate.next(now, last, period, grace);
+
+            // :: Verify
+            assertWithMessage("last: " + last)
+                    .that(next)
+                    .isEqualTo(now + grace);
+
+            testCount++;
+        }
+        assertThat(testCount).isEqualTo(3);
+    }
+
+    @Test
+    public void next_betweenGraceAndAPeriodSinceLastReading() {
+        // :: Setup
+        long now = 10_000;
+        long period = 1_000;
+        int grace = 5;
+
+        int testCount = 0;
+
+        // :: Act
+        for (long last = now - 3 * grace; last >= now - period; last -= grace) {
+            long next = Anticipate.next(now, last, period, grace);
+
+            // :: Verify
+            assertWithMessage("last: " + last)
+                    .that(next)
+                    .isEqualTo(Math.max(last + period - grace, now + grace));
+
+            testCount++;
+        }
+
+        assertThat(testCount).isEqualTo(198);
+    }
+
+    @Test
+    public void next_moreThanTwoPeriodsSinceLastReading() {
+        // :: Setup
+        long now = 10_000;
+        long period = 1_000;
+        long grace = 5;
+
+        int testCount = 0;
+
+        // :: Act
+        for (long last = now - 2 * period - grace; last >= now - 5 * period; last -= (grace * 3)) {
+            long next = Anticipate.next(now, last, period, grace);
+
+            // :: Verify
+            assertWithMessage("last: " + last + " next is at least")
+                    .that(next)
+                    .isAtLeast(now + grace);
+            assertWithMessage("last: " + last + " next is at most")
+                    .that(next)
+                    .isAtMost(now + period);
+
+            long restOfWholePeriod = Math.abs((next - last) % period);
+            if (restOfWholePeriod > period / 2) {
+                restOfWholePeriod -= period;
+            }
+            assertWithMessage("last: " + last + " is around modulus grace")
+                    .that(restOfWholePeriod)
+                    .isAtMost(2 * grace);
+
+            testCount++;
+        }
+
+        assertThat(testCount).isEqualTo(200);
+    }
+
+    // ===== Real world examples ===================================================================
+    @Test
+    public void next_realWorld1() {
+        // :: Setup
+        // 2019-06-38 11:18:49
+        long now = 1_561_893_529_622L;
+        // 2019-06-30 11:13:29
+        long last = 1_561_893_209_000L;
+        // 5 minutes
+        long period = 300_000L;
+        // 10 seconds
+        long grace = 10_000;
+
+        // :: Act
+        long next = Anticipate.next(now, last, period, grace);
+
+        // :: Verify
+        // 2019-06-30 11:18:59 (now + grace)
+        assertThat(next).isEqualTo(1_561_893_539_622L);
     }
 }
