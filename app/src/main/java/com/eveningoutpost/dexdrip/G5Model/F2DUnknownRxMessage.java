1cd2076a8cbf9158e3a2ab7ae9aa024f0903d0bd
Jamorham <jamorham@users.noreply.github.com>
2019-08-12 14:20:26 +0100

    Add: F2DUnknownRxMessage

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/G5Model/F2DUnknownRxMessage.java
@@ -0,0 +1,12 @@
+package com.eveningoutpost.dexdrip.G5Model;
+
+// jamorham
+
+public class F2DUnknownRxMessage extends BaseMessage {
+
+    public static final byte opcode = (byte) 0xD0;
+
+    public F2DUnknownRxMessage(final byte[] packet) {
+        // stub
+    }
+}
