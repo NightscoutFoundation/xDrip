43d94a43f7188e849b00b0e6c5e46f06f7fae9af
Jamorham <jamorham@users.noreply.github.com>
2019-09-06 16:23:14 +0100

    BlueJay: add device unit test scheduler

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/watch/thinjam/DebugUnitTestLogger.java
@@ -0,0 +1,43 @@
+package com.eveningoutpost.dexdrip.watch.thinjam;
+
+import com.eveningoutpost.dexdrip.Models.JoH;
+import com.eveningoutpost.dexdrip.Models.UserError;
+import com.eveningoutpost.dexdrip.UtilityModels.Inevitable;
+import com.eveningoutpost.dexdrip.watch.thinjam.io.GetURL;
+
+import lombok.RequiredArgsConstructor;
+
+// jamorham
+
+@RequiredArgsConstructor
+public class DebugUnitTestLogger {
+
+    private static final String TAG = "BlueJayMassUnit";
+
+    private static final String SERVER_ADDRESS = "http://127.0.0.1:12577/";
+
+    private final BlueJayService parent;
+
+    public void processTestSuite(final String cmd) {
+        JoH.static_toast_long("Test Suite: " + cmd);
+        UserError.Log.d(TAG, "Process called with: " + cmd);
+
+        if (cmd == null) {
+            UserError.Log.d(TAG, "Null process command received!");
+            return;
+        }
+
+        if (cmd.equals("shutdown")) {
+            parent.stopSelf();
+        }
+
+        Inevitable.task("bj-mass-prod-schedule-test" + cmd, 100, new Runnable() {
+            @Override
+            public void run() {
+                final String result = GetURL.getURL(SERVER_ADDRESS + "mass/batch/schedule/" + cmd + "/" + BlueJay.getMac());
+                UserError.Log.d(TAG, "Unit test schedule result for " + cmd + ": " + result);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/eveningoutpost/dexdrip/watch/thinjam/io/GetURL.java
@@ -0,0 +1,73 @@
+package com.eveningoutpost.dexdrip.watch.thinjam.io;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import lombok.val;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static com.eveningoutpost.dexdrip.UtilityModels.OkHttpWrapper.enableTls12OnPreLollipop;
+
+// jamorham
+
+// Just some utility methods to retrieve urls
+
+public class GetURL {
+
+    private static OkHttpClient httpClient = null;
+
+    public static String getURL(final String URL) {
+        try {
+            val response = getURLresponse(URL);
+            if (response.isSuccessful()) {
+                return response.body().string();
+            }
+        } catch (NullPointerException | IOException e) {
+            //
+        }
+        return null;
+    }
+
+    public static byte[] getURLbytes(final String URL) {
+        try {
+            val response = getURLresponse(URL);
+            if (response.isSuccessful()) {
+                return response.body().bytes();
+            }
+        } catch (NullPointerException | IOException e) {
+            //
+        }
+        return null;
+    }
+
+    // must be on background thread
+    private static Response getURLresponse(final String URL) {
+
+        if (httpClient == null) {
+            httpClient = enableTls12OnPreLollipop(new OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(20, TimeUnit.SECONDS))
+                    .build();
+        }
+
+
+        final Request request = new Request.Builder()
+                // Mozilla header facilitates compression
+                .header("User-Agent", "Mozilla/5.0")
+                .header("Connection", "close")
+                .url(URL)
+                .build();
+
+        try {
+            return httpClient.newCall(request).execute();
+        } catch (NullPointerException | IOException e) {
+            // meh fall through to default failure
+        }
+
+        return null;
+    }
+
+}
