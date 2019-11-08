770ee90d5ace94a671ce242ebc6828d892ee908b
bigdigital <bigdigital.kov@gmail.com>
2019-11-08 14:15:22 +0200

    refactor pref binding

--- app/src/main/java/com/eveningoutpost/dexdrip/watch/miband/message/DisplayControllMessage.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/watch/miband/message/DisplayControllMessage.java
@@ -1,34 +1,25 @@
 package com.eveningoutpost.dexdrip.watch.miband.message;
 
-import com.eveningoutpost.dexdrip.Models.UserError;
 import com.eveningoutpost.dexdrip.watch.miband.Const;
 
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 
-import static com.eveningoutpost.dexdrip.watch.miband.message.OperationCodes.DISPLAY_ITEM_BIT_CLOCK;
-import static com.eveningoutpost.dexdrip.watch.miband.message.OperationCodes.ENDPOINT_DISPLAY_ITEMS;
-
 public class DisplayControllMessage extends BaseMessage {
-    private static final String TAG = "MiBand_DisplayControllMessage";
+    private static final String TAG = DisplayControllMessage.class.getSimpleName();
 
     public static final int DISPLAY_ITEM_STEPS = 0;
     public static final int DISPLAY_ITEM_DISTANCE = 1;
     public static final int DISPLAY_ITEM_CALORIES = 2;
     public static final int DISPLAY_ITEM_HEART_RATE = 3;
-    public static final int DISPLAY_ITEM_BATTERY = 5;
-
-    //0a 01 00 00 01 02 03 04 05  disabled all screens
-    //0a 11 00 00 01 02 03 04 05  heart rate
+    public static final int DISPLAY_ITEM_BATTERY = 4;
 
     @Override
     public UUID getCharacteristicUUID() {
         return Const.UUID_CHARACTERISTIC_3_CONFIGURATION;
     }
 
-    public byte[] getDisplayItemsMessage(List<Integer> pages) {
-        UserError.Log.e(TAG, "Setting display items to " + ((pages == null && pages.isEmpty()) ? "none" : pages));
+    public byte[] getDisplayItemsCmd(List<Integer> pages) {
         byte[] data = OperationCodes.COMMAND_CHANGE_SCREENS.clone();
 
         if (pages != null && !pages.isEmpty()) {
