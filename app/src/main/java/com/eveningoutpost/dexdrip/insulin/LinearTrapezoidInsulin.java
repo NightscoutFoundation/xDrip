8776bf3d8f42876f52e6dc62fbb558d060f9ab11
gruoner <torsten@gruoner.ch>
2020-01-21 22:57:00 +0100

    fixed 2 bugs and a poor line of code: - removed concentration from activity calculation as discussed in #1080 - fixed a typo in activity calculation leading to a wrong prediction

--- app/src/main/java/com/eveningoutpost/dexdrip/insulin/LinearTrapezoidInsulin.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/insulin/LinearTrapezoidInsulin.java
@@ -37,18 +37,18 @@ public class LinearTrapezoidInsulin extends Insulin {
         else if ((t1 <= time) && (time < t2))
             return 1.0 + 0.5 * max * t1 - max * time;
         else if ((t2 <= time) && (time < t3))
-            return 0.5 * (time - t3) * (time - t3) * max / (t3 - t2);
+            return 0.5 * (t3 - time) * (t3 - time) * max / (t3 - t2);
         else return 0;
     }
 
     public double calculateActivity(double t) {
         double time = t - onset;
         if ((0 <= time) && (time < t1))
-            return concentration * time * max / t2;
+            return concentration * time * max / t1;
         else if ((t1 <= time) && (time < t2))
             return concentration * max;
         else if ((t2 <= time) && (time < t3))
-            return concentration * (time - t3) * max / (t3 - t2);
+            return concentration * (t3 - time) * max / (t3 - t2);
         else return 0;
     }
 
