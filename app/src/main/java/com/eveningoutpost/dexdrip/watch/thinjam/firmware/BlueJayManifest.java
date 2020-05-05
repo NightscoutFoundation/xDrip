21752aa324e167be4bfdc368845097cb81567d28
Jamorham <jamorham@users.noreply.github.com>
2020-05-05 20:50:39 +0100

    BlueJay: catch invalid manifest

--- app/src/main/java/com/eveningoutpost/dexdrip/watch/thinjam/firmware/BlueJayManifest.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/watch/thinjam/firmware/BlueJayManifest.java
@@ -3,6 +3,8 @@ package com.eveningoutpost.dexdrip.watch.thinjam.firmware;
 // jamorham
 
 import com.eveningoutpost.dexdrip.Models.JoH;
+import com.eveningoutpost.dexdrip.Models.UserError;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.annotations.Expose;
 import com.google.gson.reflect.TypeToken;
 
@@ -23,12 +25,17 @@ public class BlueJayManifest {
     @Expose
     public int version;
 
+    private static final String TAG = "BlueJayManifest";
 
     public static List<BlueJayManifest> parseToList(final String manifestString) {
         if (!emptyString(manifestString)) {
-            final Type queueType = new TypeToken<ArrayList<BlueJayManifest>>() {
-            }.getType();
-            return JoH.defaultGsonInstance().fromJson(manifestString, queueType);
+            try {
+                final Type queueType = new TypeToken<ArrayList<BlueJayManifest>>() {
+                }.getType();
+                return JoH.defaultGsonInstance().fromJson(manifestString, queueType);
+            } catch (JsonSyntaxException e) {
+                UserError.Log.e(TAG, "Exception parsing manifest: " + e);
+            }
         }
         return null;
     }
