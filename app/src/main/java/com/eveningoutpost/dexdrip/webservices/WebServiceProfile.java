e0f91257c949625a80ea38c72c1523ef68da0c7a
James Woglom <j@wogloms.net>
2020-12-29 15:10:07 -0500

    WebServiceProfile return empty array

--- app/src/main/java/com/eveningoutpost/dexdrip/webservices/WebServiceProfile.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/webservices/WebServiceProfile.java
@@ -1,17 +1,6 @@
 package com.eveningoutpost.dexdrip.webservices;
 
-import android.util.Log;
-
-import com.eveningoutpost.dexdrip.Models.JoH;
-import com.eveningoutpost.dexdrip.Models.UserError;
-import com.eveningoutpost.dexdrip.UtilityModels.Constants;
-import com.eveningoutpost.dexdrip.UtilityModels.Pref;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.JSONArray;
-
-import static com.eveningoutpost.dexdrip.Models.JoH.tolerantParseDouble;
 
 /**
  * @author James Woglom (j@wogloms.net)
@@ -22,42 +11,7 @@ public class WebServiceProfile extends BaseWebService {
     private static String TAG = "WebServiceProfile";
 
     public WebResponse request(String query) {
-        final JSONObject reply = new JSONObject();
         final JSONArray wrapper = new JSONArray();
-
-        // populate json structures
-        try {
-            reply.put("defaultProfile", "Default");
-            final JSONObject basal = new JSONObject();
-            basal.put("time", "00:00");
-            basal.put("value", "0.0");
-            basal.put("timeAsSeconds", "0");
-
-            final JSONArray basalArr = new JSONArray();
-            basalArr.put(basal);
-            final JSONObject defaultObj = new JSONObject();
-            defaultObj.put("basal", basalArr);
-            reply.put("store", defaultObj);
-            /*
-            "defaultProfile": "Default",
-            "store": {
-              "Default": {
-                "basal": [
-                  {
-                    "time": "00:00",
-                    "value": "0.0",
-                    "timeAsSeconds": "0"
-                  }
-                ],
-              }
-            },
-             */
-
-            wrapper.put(reply);
-            Log.d(TAG, "Output: " + wrapper.toString());
-        } catch (JSONException e) {
-            UserError.Log.wtf(TAG, "Got json exception: " + e);
-        }
 
         return new WebResponse(wrapper.toString());
     }
