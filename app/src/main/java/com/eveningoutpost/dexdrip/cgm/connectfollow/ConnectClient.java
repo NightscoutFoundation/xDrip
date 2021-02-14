567db2e3dc76466e8756cae1fe3e56a833e6c004
Bence Szász <32592661+benceszasz@users.noreply.github.com>
2021-02-14 10:45:16 +0100

    CareLink ConnectData HTTP communication client

---
+++ app/src/main/java/com/eveningoutpost/dexdrip/cgm/connectfollow/ConnectClient.java
@@ -0,0 +1,346 @@
+package com.eveningoutpost.dexdrip.cgm.connectfollow;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import lombok.Getter;
+import okhttp3.*;
+
+import com.eveningoutpost.dexdrip.cgm.connectfollow.messages.ConnectData;
+import com.eveningoutpost.dexdrip.cgm.connectfollow.messages.ConnectDataResult;
+import com.google.gson.*;
+
+
+
+// TODO list
+// 1) automatic token handling
+//    - request token
+//    - refresh token
+//    - remove fetch data
+// 2) add other CareLink APIs
+//    - get patient data
+//    - get device info (last update???)
+//    - generate report
+//    - others?!
+// 3) optimize getLast24hours
+//    - use device info
+// 4) HTML helper class for data extraction from responses
+// 6) other events, markers, notes
+
+public class ConnectClient {
+    protected String carelinkUsername;
+    protected String carelinkPassword;
+    protected Country carelinkCountry;
+    protected static final String CARELINK_CONNECT_SERVER_EU = "carelink.minimed.eu";
+    protected static final String CARELINK_CONNET_SERVER_US = "carelink.minimed.com";
+    protected static final String CARELINK_LANGUAGE_EN = "en";
+    protected static final String CARELINK_LOCALE_EN = "en";
+    protected static final String CARELINK_AUTH_TOKEN_COOKIE_NAME = "auth_tmp_token";
+    protected static final String CARELINK_TOKEN_VALIDTO_COOKIE_NAME = "c_token_valid_to";
+    protected static final int AUTH_EXPIRE_DEADLINE_MINUTES = 1;
+
+    protected OkHttpClient httpClient = null;
+
+    private boolean lastLoginSuccess;
+    public boolean getLastLoginSuccess(){
+        return lastLoginSuccess;
+    }
+    private boolean lastDataSuccess;
+    public boolean getLastDataSuccess(){
+        return lastDataSuccess;
+    }
+    private String lastErrorMessage;
+    public String getLastErrorMessage(){
+        return lastErrorMessage;
+    }
+    private String lastStackTraceString;
+    public String getLastStackTraceString(){
+        return lastStackTraceString;
+    }
+
+
+
+    protected enum RequestType{
+        HtmlGet(),
+        HtmlPost(),
+        Json()
+    }
+
+    public ConnectClient(String carelinkUsername, String carelinkPassword, Country carelinkCountry) {
+
+        this.carelinkUsername = carelinkUsername;
+        this.carelinkPassword = carelinkPassword;
+        this.carelinkCountry = carelinkCountry;
+
+        lastLoginSuccess = false;
+        lastDataSuccess = false;
+
+        //Create main http client with CookieJar
+        this.httpClient = new OkHttpClient.Builder()
+                .cookieJar(new SimpleOkHttpCookieJar())
+                .build();
+
+    }
+
+    protected String careLinkServer() {
+        return(this.carelinkCountry == Country.UnitedStates ? CARELINK_CONNET_SERVER_US : CARELINK_CONNECT_SERVER_EU );
+    }
+
+
+    //Authentication methods
+    protected boolean executeAuthenticationProcedure() {
+
+        Response loginSessionResponse = null;
+        Response doLoginResponse = null;
+        Response consentResponse = null;
+
+
+        lastLoginSuccess = false;
+
+        try {
+            //TODO handle login errors when occur, check results
+
+            //Clear cookies
+            ((SimpleOkHttpCookieJar) this.httpClient.cookieJar()).deleteAllCookies();
+
+            //Open login (get SessionId and SessionData)
+            loginSessionResponse = this.getLoginSession();
+
+            //Login
+            doLoginResponse = this.doLogin(loginSessionResponse);
+            loginSessionResponse.close();
+
+            //Consent
+            consentResponse = this.doConsent(doLoginResponse);
+            doLoginResponse.close();
+            consentResponse.close();
+
+            //Set login success
+            lastLoginSuccess = true;
+
+        } catch (Exception e) {
+            lastErrorMessage = e.getMessage();
+            Log.getStackTraceString(e);
+            return lastLoginSuccess;
+        }
+
+        return lastLoginSuccess;
+
+    }
+
+    protected Response getLoginSession() throws IOException {
+
+        HttpUrl url = null;
+        Request.Builder requestBuilder = null;
+
+
+        url = new HttpUrl.Builder()
+                .scheme("https")
+                .host(this.careLinkServer())
+                .addPathSegments("patient/sso/login")
+                .addQueryParameter("country", this.carelinkCountry.getCountryCode())
+                .addQueryParameter("lang", CARELINK_LANGUAGE_EN)
+                .build();
+
+        requestBuilder = new Request.Builder()
+                .url(url);
+
+        this.addHttpHeaders(requestBuilder, RequestType.HtmlGet);
+
+        return this.httpClient.newCall(requestBuilder.build()).execute();
+
+    }
+
+    protected Response doLogin(Response loginSessionResponse) throws IOException {
+
+        HttpUrl url = null;
+        Request.Builder requestBuilder = null;
+        RequestBody form = null;
+
+        form = new FormBody.Builder()
+                .add("sessionID", loginSessionResponse.request().url().queryParameter("sessionID"))
+                .add("sessionData", loginSessionResponse.request().url().queryParameter("sessionData"))
+                .add("locale", CARELINK_LOCALE_EN)
+                .add("action", "login")
+                .add("username", this.carelinkUsername)
+                .add("password", this.carelinkPassword)
+                .add("actionButton", "Log in")
+                .build();
+
+        url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("mdtlogin.medtronic.com")
+                .addPathSegments("mmcl/auth/oauth/v2/authorize/login")
+                .addQueryParameter("locale", CARELINK_LOCALE_EN)
+                .addQueryParameter("country", this.carelinkCountry.getCountryCode())
+                .build();
+
+        requestBuilder = new Request.Builder()
+                .url(url)
+                .post(form);
+
+        this.addHttpHeaders(requestBuilder, RequestType.HtmlGet);
+
+        return this.httpClient.newCall(requestBuilder.build()).execute();
+
+    }
+
+    protected Response doConsent(Response doLoginResponse) throws IOException {
+
+        Request.Builder requestBuilder = null;
+        RequestBody form = null;
+        String doLoginRespBody = null;
+
+        doLoginRespBody = doLoginResponse.body().string();
+
+        //Extract data for consent
+        String consentUrl = this.extractResponseData(doLoginRespBody, "(form action=\")(.*)(\" method=\"POST\")", 2);
+        String consentSessionData = this.extractResponseData(doLoginRespBody, "(<input type=\"hidden\" name=\"sessionData\" value=\")(.*)(\">)", 2);
+        String consentSessionId = this.extractResponseData(doLoginRespBody, "(<input type=\"hidden\" name=\"sessionID\" value=\")(.*)(\">)", 2);
+
+        //Send consent
+        form = new FormBody.Builder()
+                .add("action", "consent")
+                .add("sessionID", consentSessionId)
+                .add("sessionData", consentSessionData)
+                .add("response_type", "code")
+                .add("response_mode", "query")
+                .build();
+
+        requestBuilder = new Request.Builder()
+                .url(consentUrl)
+                .post(form);
+
+        this.addHttpHeaders(requestBuilder, RequestType.HtmlPost);
+
+        return this.httpClient.newCall(requestBuilder.build()).execute();
+
+    }
+
+    protected String extractResponseData(String respBody, String groupRegex, int groupIndex) {
+
+        String responseData = null;
+        Matcher responseDataMatcher = null;
+
+        responseDataMatcher = Pattern.compile(groupRegex).matcher(respBody);
+        if(responseDataMatcher.find()) {
+            responseData = responseDataMatcher.group(groupIndex);
+        }
+
+        return responseData;
+
+    }
+
+    protected String getAuthorizationToken() {
+
+
+        //New token is needed: no token or about to expire => execute authentication
+//        if(!((SimpleOkHttpCookieJar) httpClient.cookieJar()).contains(CARELINK_AUTH_TOKEN_COOKIE_NAME) ||
+//                !((SimpleOkHttpCookieJar) httpClient.cookieJar()).contains(CARELINK_TOKEN_VALIDTO_COOKIE_NAME) ||
+//                !((new Date(Date.parse(((SimpleOkHttpCookieJar) httpClient.cookieJar()).getCookies(CARELINK_TOKEN_VALIDTO_COOKIE_NAME).get(0).value())))
+//                        .after(new Date(new Date(System.currentTimeMillis()).getTime() + AUTH_EXPIRE_DEADLINE_MINUTES * 60000))
+//                )){
+        if(!this.executeAuthenticationProcedure()){
+            return null;
+        };
+//        }
+
+
+        //there can be only one
+        return "Bearer" + " " + ((SimpleOkHttpCookieJar) httpClient.cookieJar()).getCookies(CARELINK_AUTH_TOKEN_COOKIE_NAME).get(0).value();
+
+
+    }
+
+
+    //CareLink data APIs
+    public ConnectDataResult getLast24Hours() {
+
+        HttpUrl url = null;
+        Request.Builder requestBuilder = null;
+        String authToken = null;
+        String currentTime = null;
+        String connectDataString = null;
+        ConnectDataResult connectDataResult = null;
+        Response response = null;
+
+
+        lastDataSuccess = false;
+        connectDataResult = new ConnectDataResult();
+
+        //Get auth token
+        authToken= this.getAuthorizationToken();
+        if(authToken != null){
+
+            // TODO current time ?
+            currentTime = String.valueOf(System.currentTimeMillis());
+
+            //Get connect data of last 24 hours
+            url = new HttpUrl.Builder()
+                    .scheme("https")
+                    .host(this.careLinkServer())
+                    .addPathSegments("patient/connect/data")
+                    .addQueryParameter("cpSerialNumber", "NONE")
+                    .addQueryParameter("msgType", "last24hours")
+                    .addQueryParameter("requestTime", currentTime)
+                    .build();
+
+            requestBuilder = new Request.Builder()
+                    .url(url)
+                    .addHeader("Authorization", authToken);
+
+            this.addHttpHeaders(requestBuilder, RequestType.Json);
+
+            //Get and convert data
+            try {
+                response = this.httpClient.newCall(requestBuilder.build()).execute();
+                connectDataResult.responseCode = response.code();
+                if(response.isSuccessful()) {
+                    connectDataString = response.body().string();
+                    connectDataResult.connectData = (new GsonBuilder().create()).fromJson(connectDataString, ConnectData.class);
+                    //only if there is actual data
+                    if (connectDataResult.connectData != null) lastDataSuccess = true;
+                }
+                response.close();
+            } catch (Exception e) {
+                lastErrorMessage = e.getMessage();
+                lastStackTraceString = Log.getStackTraceString(e);
+            }
+
+        }
+
+        //set data request success
+        connectDataResult.success = lastDataSuccess;
+
+        return connectDataResult;
+
+    }
+
+    protected void addHttpHeaders(Request.Builder requestBuilder, RequestType type) {
+
+        requestBuilder
+                .addHeader("Accept-Language", "en;q=0.9, *;q=0.8")
+                .addHeader("Connection", "keep-alive")
+                .addHeader("sec-ch-ua", "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+
+        switch(type) {
+            case Json:
+                requestBuilder.addHeader("Accept", "application/json, text/plain, */*");
+                requestBuilder.addHeader("Content-Type", "application/json; charset=utf-8");
+                break;
+            case HtmlGet:
+                requestBuilder.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            case HtmlPost:
+                requestBuilder.addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+                requestBuilder.addHeader("Content-Type", "application/x-www-form-urlencoded");
+                break;
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/eveningoutpost/dexdrip/cgm/connectfollow/SimpleOkHttpCookieJar.java
@@ -0,0 +1,80 @@
+package com.eveningoutpost.dexdrip.cgm.connectfollow;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SimpleOkHttpCookieJar implements CookieJar {
+
+    private List<Cookie> storage = new ArrayList<>();
+
+    @Override
+    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+        storage.addAll(cookies);
+    }
+
+    @Override
+    public List<Cookie> loadForRequest(HttpUrl url) {
+
+        List<Cookie> cookies = new ArrayList<>();
+
+        // Remove expired Cookies
+        removeExpiredCookies();
+
+        // Only return matching Cookies
+        for (int i = 0; i < storage.size(); i++) {
+            if(storage.get(i).matches(url)) {
+                cookies.add(storage.get(i));
+            }
+        }
+
+        return cookies;
+    }
+
+    public List<Cookie> getCookies(String name) {
+
+        List<Cookie> cookies = new ArrayList<>();
+
+        // Remove expired Cookies
+        removeExpiredCookies();
+
+        // Only return matching Cookies
+        for (int i = 0; i < storage.size(); i++) {
+            if(storage.get(i).name().equals(name)) {
+                cookies.add(storage.get(i));
+            }
+        }
+
+        return cookies;
+
+    }
+
+    public boolean contains(String name) {
+        return (getCookies(name).size() > 0);
+    }
+
+    public void deleteCookie(String name) {
+        for (int i = 0; i < storage.size(); i++) {
+            if(storage.get(i).name() == name) {
+                storage.remove(i);
+            }
+        }
+    }
+
+    public void deleteAllCookies() {
+        storage.clear();
+    }
+
+    private void removeExpiredCookies(){
+        for (int i = 0; i < storage.size(); i++) {
+            if(storage.get(i).expiresAt() < System.currentTimeMillis()) {
+                storage.remove(i);
+            }
+        }
+    }
+
+}
