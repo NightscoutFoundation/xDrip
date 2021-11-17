28cf1f148b2c6787b19ef4f8c2cc067e3f7bff02
JamOrHam <jamorham@users.noreply.github.com>
2021-11-17 15:41:58 +0000

    Merge pull request #1867 from marciogranzotto/fix-number-wall-bg
    Fix NumberWall Background image

--- app/src/main/java/com/eveningoutpost/dexdrip/ui/helpers/BitmapUtil.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/ui/helpers/BitmapUtil.java
@@ -15,52 +15,23 @@ import android.net.Uri;
 
 import com.eveningoutpost.dexdrip.Models.JoH;
 import com.eveningoutpost.dexdrip.UtilityModels.Pref;
+import com.eveningoutpost.dexdrip.utils.FileUtils;
 import com.eveningoutpost.dexdrip.xdrip;
 
+import java.io.File;
 import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class BitmapUtil {
 
     // note: this recycles the source bitmap - TODO make this more generic
-    public static Bitmap getTiled(final Bitmap source, final int xSize, final int ySize, final boolean tile, final String background_file) {
+    public static Bitmap getTiled(final Bitmap source, final int xSize, final int ySize, final boolean tile, final String backgroundFile) {
         Bitmap tiledBitmap = null;
-        if (background_file != null) {
-            try {
-
-                tiledBitmap = BitmapLoader.bitmapFromBundleCache(background_file);
-                if (tiledBitmap == null) {
-                    android.util.Log.d("NumberWall", "Regenerating image");
-
-                    final Uri background_uri = Uri.parse(background_file);
-
-                    FileDescriptor fileDescriptor = xdrip.getAppContext().getContentResolver().openFileDescriptor(background_uri, "r").getFileDescriptor();
-                    final Bitmap image_bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-                    Bitmap rotated_bitmap = image_bitmap;
-
-                    final Matrix image_matrix = new Matrix();
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                        fileDescriptor = xdrip.getAppContext().getContentResolver().openFileDescriptor(background_uri, "r").getFileDescriptor(); // reset
-                        final ExifInterface exif = new ExifInterface(fileDescriptor);
-                        int rotation = exifOrientationToDegrees(exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL));
-                        android.util.Log.d("NumberWall", "Rotation: " + rotation);
-
-                        if (rotation != 0) {
-                            image_matrix.preRotate(rotation);
-                            rotated_bitmap = Bitmap.createBitmap(image_bitmap, 0, 0, image_bitmap.getWidth(), image_bitmap.getHeight(), image_matrix, true);
-                            image_bitmap.recycle();
-                        }
-                    }
-
-                    tiledBitmap = getBestCroppedScaled(rotated_bitmap, xSize, ySize);
-                    BitmapLoader.saveBitmapAsBundle(background_file, Bitmap.createBitmap(tiledBitmap));
-                } else {
-                    tiledBitmap = Bitmap.createBitmap(tiledBitmap); // make a copy
-                    android.util.Log.d("NumberWall", "cache hit");
-                }
-            } catch (Exception e) {
-                // cannot load bitmap
-                android.util.Log.e("NumberWall", "Cannot load bitmap: " + e);
-            }
+        if (backgroundFile != null) {
+            tiledBitmap = getBackgroundBitmap(xSize, ySize, backgroundFile);
         }
         if (tiledBitmap == null) {
             tiledBitmap = Bitmap.createBitmap(xSize, ySize, Bitmap.Config.ARGB_8888);
@@ -74,16 +45,111 @@ public class BitmapUtil {
                 drawable.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
                 drawable.draw(canvas);
             } else {
-                double y_ratio = JoH.tolerantParseDouble(Pref.getString("numberwall_y_param", ""), 50d) / 100d; // TODO move to method signature
-                int yoffset = Math.max(0, (int) ((getScreenDpi() * y_ratio * 1.2d) - (getScreenDpi() * 0.30d)));
-                final double spacer_ratio = JoH.tolerantParseDouble(Pref.getString("numberwall_s_param", ""), 10d) / 100d;
-                int xoffset = Math.max(0, (int) ((getScreenDpi() * spacer_ratio * 1.2d) - (getScreenDpi() * 0.30d)));
+                double yRatio = JoH.tolerantParseDouble(Pref.getString("numberwall_y_param", ""), 50d) / 100d; // TODO move to method signature
+                int yOffset = Math.max(0, (int) ((getScreenDpi() * yRatio * 1.2d) - (getScreenDpi() * 0.30d)));
+                final double spacerRatio = JoH.tolerantParseDouble(Pref.getString("numberwall_s_param", ""), 10d) / 100d;
+                int xOffset = Math.max(0, (int) ((getScreenDpi() * spacerRatio * 1.2d) - (getScreenDpi() * 0.30d)));
 
-                canvas.drawBitmap(source, xoffset, yoffset, new Paint());
+                canvas.drawBitmap(source, xOffset, yOffset, new Paint());
             }
             source.recycle();
         } // returns blank bitmap if source is null
         return tiledBitmap;
+    }
+
+    public static Bitmap getBackgroundBitmap(final int xSize, final int ySize, final String backgroundFile) {
+        Bitmap tiledBitmap = null;
+        FileInputStream inputStream = null;
+        try {
+            tiledBitmap = BitmapLoader.bitmapFromBundleCache(backgroundFile);
+            if (tiledBitmap == null) {
+                android.util.Log.d("NumberWall", "Regenerating image");
+
+                final Uri backgroundUri = Uri.parse(backgroundFile);
+
+                Bitmap imageBitmap = null;
+                if (backgroundFile.startsWith("content://")) {
+                    FileDescriptor fileDescriptor = xdrip.getAppContext().getContentResolver().openFileDescriptor(backgroundUri, "r").getFileDescriptor();
+                    imageBitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                } else {
+                    inputStream = new FileInputStream(backgroundFile);
+                    imageBitmap = BitmapFactory.decodeStream(inputStream);
+                    inputStream.close();
+                }
+
+                Bitmap rotatedBitmap = imageBitmap;
+                final Matrix imageMatrix = new Matrix();
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                    ExifInterface exif = null;
+                    if (backgroundFile.startsWith("content://")) {
+                        FileDescriptor fileDescriptor = xdrip.getAppContext().getContentResolver().openFileDescriptor(backgroundUri, "r").getFileDescriptor(); // reset
+                        exif = new ExifInterface(fileDescriptor);
+                    } else {
+                        inputStream = new FileInputStream(backgroundFile);
+                        exif = new ExifInterface(inputStream);
+                    }
+                    int rotation = exifOrientationToDegrees(exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL));
+                    android.util.Log.d("NumberWall", "Rotation: " + rotation);
+
+                    if (rotation != 0) {
+                        imageMatrix.preRotate(rotation);
+                        rotatedBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(), imageBitmap.getHeight(), imageMatrix, true);
+                        imageBitmap.recycle();
+                    }
+                }
+
+                tiledBitmap = getBestCroppedScaled(rotatedBitmap, xSize, ySize);
+                BitmapLoader.saveBitmapAsBundle(backgroundFile, Bitmap.createBitmap(tiledBitmap));
+            } else {
+                tiledBitmap = Bitmap.createBitmap(tiledBitmap); // make a copy
+                android.util.Log.d("NumberWall", "cache hit");
+            }
+        } catch (Exception e) {
+            // cannot load bitmap
+            android.util.Log.e("NumberWall", "Cannot load bitmap: " + e);
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    android.util.Log.e("NumberWall", "Error closing input stream", e);
+                }
+            }
+        }
+        return tiledBitmap;
+    }
+
+    public static String copyBackgroundImage(Uri sourceUri, File destinationFolder) {
+        // delete directory with older files
+        FileUtils.deleteDirWithFiles(destinationFolder);
+        // recreate directory
+        destinationFolder.mkdirs();
+        String destinationFileName = "background" + new Date().getTime() + ".png"; // unique name to avoid caching
+        File destination = new File(destinationFolder, destinationFileName);
+
+        Bitmap sourceImage = getBackgroundBitmap(getScreenWidth(), getScreenHeight(), sourceUri.toString());
+        String output = destination.getPath();
+        if (sourceImage != null) {
+            FileOutputStream out = null;
+            try {
+                out = new FileOutputStream(destination);
+                sourceImage.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+            } catch (IOException e) {
+                android.util.Log.e("NumberWall", "Error copying background image", e);
+                // fallback to original
+                output = sourceUri.toString();
+            } finally {
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        android.util.Log.e("NumberWall", "Error closing output stream", e);
+                    }
+                }
+            }
+        }
+        return output;
     }
 
 
