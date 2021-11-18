b4eff0629d7c2ae5ab8ada8b9e54a577b5e1afb2
Navid <mymark14@hotmail.com>
2021-11-17 22:46:15 -0500

    Sound file page title is back as a string

--- app/src/main/java/com/eveningoutpost/dexdrip/EditAlertActivity.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/EditAlertActivity.java
@@ -589,12 +589,12 @@ public class EditAlertActivity extends ActivityWithMenu {
         buttonalertMp3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-                builder.setTitle("")
-                        .setItems(R.array.alertType, new DialogInterface.OnClickListener() {
+                builder.setTitle(getString(R.string.select_new_sound))
+                        .setItems(R.array.alertSound, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 if (which == 0) {
                                     Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-                                    intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select tone for Alerts:");
+                                    intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, getString(R.string.choose_alert_sound_file));
                                     intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, true);
                                     intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true);
                                     intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALL);
