8101dfa3fd48fea9bc15cf9db0c0b832ff79f556
Jordi <jordipbou@protonmail.com>
2021-11-29 19:15:35 +0100

    Update Libre2RawValue.java
    Formatted.

--- app/src/main/java/com/eveningoutpost/dexdrip/Models/Libre2RawValue.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Models/Libre2RawValue.java
@@ -1,29 +1,29 @@
 package com.eveningoutpost.dexdrip.Models;
 
 
-        import android.provider.BaseColumns;
-        import android.util.Log;
+import android.provider.BaseColumns;
+import android.util.Log;
 
-        import com.activeandroid.annotation.Column;
-        import com.activeandroid.annotation.Table;
-        import com.activeandroid.query.Select;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
 
-        import java.util.Date;
-        import java.util.List;
+import java.util.Date;
+import java.util.List;
 
-        import com.google.gson.GsonBuilder;
-        import com.google.gson.annotations.Expose;
-        import org.json.JSONException;
-        import org.json.JSONObject;
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.Expose;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 @Table(name = "Libre2RawValue2", id = BaseColumns._ID)
 public class Libre2RawValue extends PlusModel {
     private static final String TAG = "libre2rawvalue";
 
     static final String[] schema = {
-            "DROP TABLE Libre2RawValue;",
-            "CREATE TABLE Libre2RawValue2 (_id INTEGER PRIMARY KEY AUTOINCREMENT, ts INTEGER, serial STRING, glucose REAL);",
-            "CREATE INDEX index_Libre2RawValue2_ts on Libre2RawValue2(ts);"
+        "DROP TABLE Libre2RawValue;",
+        "CREATE TABLE Libre2RawValue2 (_id INTEGER PRIMARY KEY AUTOINCREMENT, ts INTEGER, serial STRING, glucose REAL);",
+        "CREATE INDEX index_Libre2RawValue2_ts on Libre2RawValue2(ts);"
     };
 
     @Column(name = "serial", index = true)
@@ -84,8 +84,8 @@ public class Libre2RawValue extends PlusModel {
     }
 
     public static Libre2RawValue fromJSON(String json) {
-        NewLibre2RawValue nl2rv = new GsonBuilder ().excludeFieldsWithoutExposeAnnotation ().create().fromJson(json, NewLibre2RawValue.class);
-        Libre2RawValue l2rv = new Libre2RawValue ();
+        NewLibre2RawValue nl2rv = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create().fromJson(json, NewLibre2RawValue.class);
+        Libre2RawValue l2rv = new Libre2RawValue();
         l2rv.serial = nl2rv.serial;
         l2rv.glucose = nl2rv.glucose;
         l2rv.timestamp = nl2rv.timestamp;
