51a4a72848e801244ef815f8e27e8208df440ace
tolot27 <mathias@walter.gd>
2022-02-08 21:41:58 +0100

    release MediaPlayer correctly in MiBandService and Reminders

--- app/src/main/java/com/eveningoutpost/dexdrip/Reminders.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Reminders.java
@@ -17,7 +17,6 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -494,34 +493,24 @@ public class Reminders extends ActivityWithRecycler implements SensorEventListen
         showReminderDialog(v, null, 0);
     }
 
-    private synchronized MediaPlayer playSelectedSound() {
-        return JoH.playSoundUri((selectedSound != null) ? selectedSound : JoH.getResourceURI(R.raw.reminder_default_notification));
+    private synchronized void playSelectedSound() {
+        JoH.playSoundUri((selectedSound != null) ? selectedSound : JoH.getResourceURI(R.raw.reminder_default_notification));
     }
 
     public void chooseReminderSound(View v) {
-        final MediaPlayer player = playSelectedSound();
+        playSelectedSound();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Select new sound source")
                 .setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
-                        try {
-                            player.stop();
-                            player.release();
-                        } catch (Exception e) {
-                            //
-                        }
+                        JoH.stopSoundUri();
                     }
                 })
                 .setItems(R.array.reminderAlertType, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        try {
-                            player.stop();
-                            player.release();
-                        } catch (Exception e) {
-                            //
-                        }
+                        JoH.stopSoundUri();
                         if (which == 0) {
                             final Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
                             intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, xdrip.getAppContext().getString(R.string.select_tone_alert));
