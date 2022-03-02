c439b5498b8bf923ef301579796f1919729b17ca
Jamorham <jamorham@users.noreply.github.com>
2022-03-02 11:27:43 +0000

    Improve TranslationTest

--- app/src/test/java/com/eveningoutpost/dexdrip/ui/TranslationTest.java
+++ app/src/test/java/com/eveningoutpost/dexdrip/ui/TranslationTest.java
@@ -1,14 +1,20 @@
 package com.eveningoutpost.dexdrip.ui;
 
 
-import android.content.res.Configuration;
-
 import com.eveningoutpost.dexdrip.R;
 import com.eveningoutpost.dexdrip.RobolectricTestWithConfig;
 import com.eveningoutpost.dexdrip.xdrip;
 
 import org.junit.Test;
+import org.robolectric.RuntimeEnvironment;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Locale;
@@ -18,6 +24,7 @@ import java.util.TreeSet;
 import lombok.val;
 
 import static com.google.common.truth.Truth.assertWithMessage;
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class TranslationTest extends RobolectricTestWithConfig {
 
@@ -27,12 +34,42 @@ public class TranslationTest extends RobolectricTestWithConfig {
 
 
     @Test
-    public void testFormatStrings() {
-
-        final Configuration config = xdrip.getAppContext().getResources().getConfiguration();
+    public void testFormatStrings() throws IOException {
+        val config = xdrip.getAppContext().getResources().getConfiguration();
         val internal = xdrip.getAppContext().getResources().getStringArray(R.array.LocaleChoicesValues);
         val extra = new String[]{"ar", "cs", "de", "el", "en", "es", "fi", "fr", "he", "hr", "it", "iw", "ja", "ko", "nb", "nl", "no", "pl", "pt", "ro", "ru", "sk", "sl", "sv", "tr", "zh"};
+        val inset = "^values-";
         Set<String> locales = new TreeSet<>(Arrays.asList(internal));
+        class ResourceLocaleParser implements FileVisitor<Path> {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attributes) {
+                val s = dir.getFileName().toString();
+                if (s.matches(inset + "[a-z][a-z]($|-r[a-zA-Z][a-zA-Z]$)")) {
+                    val locale = s.replaceFirst(inset, "").replace("-r", "-");
+                    locales.add(locale);
+                }
+                return CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+                return CONTINUE;
+            }
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException e) {
+                return CONTINUE;
+            }
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException e) {
+                return CONTINUE;
+            }
+        }
+
+        val resourcePath = RuntimeEnvironment.application
+                .getPackageResourcePath()
+                .replaceFirst("/apk_for_local_test/.*","/merged-not-compiled-resources");
+        val size = locales.size();
+        Files.walkFileTree(Paths.get(resourcePath), new ResourceLocaleParser());
+        assertWithMessage("No locales added from resources - this seems unlikely").that(locales.size()).isGreaterThan(size);
         locales.addAll(Arrays.asList(extra));
         String fmt;
         String result;
@@ -40,7 +77,8 @@ public class TranslationTest extends RobolectricTestWithConfig {
         for (val language : locales) {
 
             System.out.println("Trying choice patterns for language: " + language);
-            config.setLocale(new Locale(language, "", ""));
+            val langCountry = language.split("-");
+            config.setLocale(new Locale(langCountry[0], langCountry.length > 1 ? langCountry[1] : "", ""));
             xdrip.getAppContext().getResources().updateConfiguration(config, xdrip.getAppContext().getResources().getDisplayMetrics());
 
             try {
@@ -49,6 +87,7 @@ public class TranslationTest extends RobolectricTestWithConfig {
                 result = MessageFormat.format(fmt, 123);
                 assertWithMessage("minutes_ago choice message format failed to contain value").that(result).contains("123");
             } catch (IllegalArgumentException e) {
+                restoreLocale();
                 throw new RuntimeException("Failed minutes ago test with language " + language + " with exception: " + e);
             }
 
@@ -58,13 +97,18 @@ public class TranslationTest extends RobolectricTestWithConfig {
                 result = MessageFormat.format(fmt, 123.4f);
                 assertWithMessage("expires_days choice message format failed to contain value").that(result).contains("123.4");
             } catch (IllegalArgumentException e) {
+                restoreLocale();
                 throw new RuntimeException("Failed expires days test with language " + language + " with exception: " + e);
             }
         }
 
+        restoreLocale();
+    }
+
+    private void restoreLocale() {
         // restore after test
+        val config = xdrip.getAppContext().getResources().getConfiguration();
         config.setLocale(new Locale("en", "", ""));
         xdrip.getAppContext().getResources().updateConfiguration(config, xdrip.getAppContext().getResources().getDisplayMetrics());
-
     }
 }
