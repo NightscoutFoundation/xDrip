4173437074e4c5c1c307dd57d9658a6da7bf065c
Jamorham <jamorham@users.noreply.github.com>
2022-06-02 14:24:25 +0000

    UIBased: initial commit

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/DexCollectionType.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/DexCollectionType.java
@@ -5,6 +5,7 @@ import com.eveningoutpost.dexdrip.Services.DexShareCollectionService;
 import com.eveningoutpost.dexdrip.Services.DoNothingService;
 import com.eveningoutpost.dexdrip.Services.G5CollectionService;
 import com.eveningoutpost.dexdrip.Services.Ob1G5CollectionService;
+import com.eveningoutpost.dexdrip.Services.UiBasedCollector;
 import com.eveningoutpost.dexdrip.Services.WifiCollectionService;
 import com.eveningoutpost.dexdrip.UtilityModels.Pref;
 import com.eveningoutpost.dexdrip.cgm.medtrum.MedtrumCollectionService;
@@ -40,6 +41,7 @@ public enum DexCollectionType {
     NSFollow("NSFollower"),
     SHFollow("SHFollower"),
     Medtrum("Medtrum"),
+    UiBased("UiBased"),
     Disabled("Disabled"),
     Mock("Mock"),
     Manual("Manual"),
@@ -53,6 +55,7 @@ public enum DexCollectionType {
     private static final HashSet<DexCollectionType> usesXbridge = new HashSet<>();
     private static final HashSet<DexCollectionType> usesFiltered = new HashSet<>();
     private static final HashSet<DexCollectionType> usesLibre = new HashSet<>();
+    private static final HashSet<DexCollectionType> isPassive = new HashSet<>();
     private static final HashSet<DexCollectionType> usesBattery = new HashSet<>();
     private static final HashSet<DexCollectionType> usesDexcomRaw = new HashSet<>();
     private static final HashSet<DexCollectionType> usesTransmitterBattery = new HashSet<>();
@@ -75,6 +78,7 @@ public enum DexCollectionType {
         Collections.addAll(usesXbridge, DexbridgeWixel, WifiDexBridgeWixel);
         Collections.addAll(usesFiltered, DexbridgeWixel, WifiDexBridgeWixel, DexcomG5, WifiWixel, Follower, Mock); // Bluetooth and Wifi+Bluetooth need dynamic mode
         Collections.addAll(usesLibre, LimiTTer, LibreAlarm, LimiTTerWifi, LibreWifi, LibreReceiver);
+        Collections.addAll(isPassive, NSEmulator, NSFollow, SHFollow, LibreReceiver, UiBased);
         Collections.addAll(usesBattery, BluetoothWixel, DexbridgeWixel, WifiBlueToothWixel, WifiDexBridgeWixel, Follower, LimiTTer, LibreAlarm, LimiTTerWifi, LibreWifi); // parakeet separate
         Collections.addAll(usesDexcomRaw, BluetoothWixel, DexbridgeWixel, WifiWixel, WifiBlueToothWixel, DexcomG5, WifiDexBridgeWixel, Mock);
         Collections.addAll(usesTransmitterBattery, WifiWixel, BluetoothWixel, DexbridgeWixel, WifiBlueToothWixel, WifiDexBridgeWixel); // G4 transmitter battery
@@ -194,6 +198,8 @@ public enum DexCollectionType {
                 return NightscoutFollowService.class;
             case SHFollow:
                 return ShareFollowService.class;
+            case UiBased:
+                return UiBasedCollector.class;
             default:
                 return DexCollectionService.class;
         }
@@ -269,6 +275,8 @@ public enum DexCollectionType {
                 return "Nightscout";
             case SHFollow:
                 return "Share";
+            case UiBased:
+                return "UI Based";
 
             default:
                 return dct.name();
@@ -299,6 +307,10 @@ public enum DexCollectionType {
         }
     }
 
+    public boolean isPassive() {
+        return isPassive.contains(this);
+    }
+
     public long getSamplePeriod() {
         return getCollectorSamplePeriod(this);
     }
@@ -309,13 +321,14 @@ public enum DexCollectionType {
                 return 300_000; // 5 minutes
         }
     }
+
     public static long getCurrentSamplePeriod() {
-       return getDexCollectionType().getSamplePeriod();
+        return getDexCollectionType().getSamplePeriod();
     }
 
     public static long getCurrentDeduplicationPeriod() {
         final long period = getDexCollectionType().getSamplePeriod();
-        return period - (period / 5); // TODO this needs more validation
+        return period - (period / 6); // TODO this needs more validation
     }
 
     public static int getCurrentSamplesForPeriod(final long periodMs) {
--- app/src/main/java/com/eveningoutpost/dexdrip/utils/Preferences.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/Preferences.java
@@ -66,6 +66,7 @@ import com.eveningoutpost.dexdrip.Services.BluetoothGlucoseMeter;
 import com.eveningoutpost.dexdrip.Services.DexCollectionService;
 import com.eveningoutpost.dexdrip.Services.G5BaseService;
 import com.eveningoutpost.dexdrip.Services.PlusSyncService;
+import com.eveningoutpost.dexdrip.Services.UiBasedCollector;
 import com.eveningoutpost.dexdrip.UtilityModels.BgGraphBuilder;
 import com.eveningoutpost.dexdrip.UtilityModels.CollectionServiceStarter;
 import com.eveningoutpost.dexdrip.UtilityModels.Constants;
@@ -420,6 +421,8 @@ public class Preferences extends BasePreferenceActivity implements SearchPrefere
                 }
             }
         };
+
+        UiBasedCollector.onEnableCheckPermission(this);
     }
 
     @Override
@@ -455,6 +458,8 @@ public class Preferences extends BasePreferenceActivity implements SearchPrefere
     }
 
 
+    private final SharedPreferences.OnSharedPreferenceChangeListener uiPrefListener = UiBasedCollector.getListener(this);
+
     @Override
     protected void onResume()
     {
@@ -466,6 +471,7 @@ public class Preferences extends BasePreferenceActivity implements SearchPrefere
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(LeFunEntry.prefListener);
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(MiBandEntry.prefListener);
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(BlueJayEntry.prefListener);
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(uiPrefListener);
         LocalBroadcastManager.getInstance(this).registerReceiver(mibandStatusReceiver,
                 new IntentFilter(Intents.PREFERENCE_INTENT));
     }
@@ -477,6 +483,7 @@ public class Preferences extends BasePreferenceActivity implements SearchPrefere
         PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(LeFunEntry.prefListener);
         PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(MiBandEntry.prefListener);
         PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(BlueJayEntry.prefListener);
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(uiPrefListener);
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mibandStatusReceiver);
         pFragment = null;
         super.onPause();
