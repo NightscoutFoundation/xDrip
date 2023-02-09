53deaa768fa03e60a473b3335c7fdfc07c128378
Navid <Navid200@users.noreply.github.com>
2023-02-09 13:44:22 -0500

    Highlight transmitter days for modified transmitters as well.

--- app/src/main/java/com/eveningoutpost/dexdrip/services/Ob1G5CollectionService.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/services/Ob1G5CollectionService.java
@@ -2167,15 +2167,22 @@ public class Ob1G5CollectionService extends G5BaseService {
                 if (!battery_status.equals("OK"))
                     l.add(new StatusItem("Transmitter Status", battery_status, BAD));
             }
-            Highlight TX_dys_highlight; // Transmitter Days highlight
+            Highlight TX_dys_highlight = NORMAL; // Transmitter Days highlight
             final int TX_dys = DexTimeKeeper.getTransmitterAgeInDays(tx_id); // Transmitter days
-            if (vr != null && (FirmwareCapability.isTransmitterRawCapable(getTransmitterID())) || TX_dys < 69) {
-                // Transmitter days < 69 or G5 or old G6 or modified Firefly
-                TX_dys_highlight = NORMAL;
-            } else if (TX_dys < 100) { // Unmodified Firefly with 68 < Transmitter days < 100
-                TX_dys_highlight = NOTICE;
-            } else { // Unmodified Firefly with transmitter days > 99
-                TX_dys_highlight = BAD;
+            if (vr != null && FirmwareCapability.isTransmitterModified(getTransmitterID())) { // Modified Firefly
+                if (TX_dys < 149) { // Transmitter days < 149
+                } else if (TX_dys < 180) { // 148 < Transmitter days < 180
+                    TX_dys_highlight = NOTICE;
+                } else { // Transmitter days > 179
+                    TX_dys_highlight = BAD;
+                }
+            } else if (vr != null && FirmwareCapability.isTransmitterRawIncapable(getTransmitterID())) { // Unmodified Firefly
+                if (TX_dys < 69) { // Transmitter days < 69
+                } else if (TX_dys < 100) { // 68 < Transmitter days < 100
+                    TX_dys_highlight = NOTICE;
+                } else { // Transmitter days > 99
+                    TX_dys_highlight = BAD;
+                }
             }
             l.add(new StatusItem("Transmitter Days", parsedBattery.daysEstimate(), TX_dys_highlight));
             l.add(new StatusItem("Voltage A", parsedBattery.voltageA(), parsedBattery.voltageAWarning() ? BAD : NORMAL));
