8fad514cc7ed898b04f8180fd083cca78ecb7acc
Navid <Navid200@users.noreply.github.com>
2024-01-02 20:30:30 -0500

    Cleanup2

--- app/src/main/java/com/eveningoutpost/dexdrip/ui/MicroStatusImpl.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/ui/MicroStatusImpl.java
@@ -4,12 +4,8 @@ package com.eveningoutpost.dexdrip.ui;
  * Created by jamorham on 29/09/2017.
  */
 
-import static com.eveningoutpost.dexdrip.services.Ob1G5CollectionService.getTransmitterID;
-
 import androidx.databinding.BaseObservable;
 
-import com.eveningoutpost.dexdrip.g5model.FirmwareCapability;
-import com.eveningoutpost.dexdrip.utilitymodels.Pref;
 import com.eveningoutpost.dexdrip.utils.DexCollectionType;
 
 public class MicroStatusImpl extends BaseObservable implements MicroStatus {
