169f5725a82e9021a955453a4ace58a61c8171c1
Navid <Navid200@users.noreply.github.com>
2024-02-26 09:24:45 -0500

    Show download treatment for nsfollow only

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/Preferences.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/Preferences.java
@@ -1256,7 +1256,7 @@ public class Preferences extends BasePreferenceActivity implements SearchPrefere
             }
 
 
-            final Preference nsFollowDownload = findPreference("nsfollow_download_treatments");
+            final Preference nsFollowDownload = findPreference("nsfollow_download_treatments_screen");
             final Preference nsFollowUrl = findPreference("nsfollow_url");
             final Preference nsFollowLag = findPreference("nsfollow_lag"); // Show the Nightscout follow wake delay setting only when NS follow is the data source
             bindPreferenceSummaryToValue(findPreference("nsfollow_lag")); // Show the selected value as summary
