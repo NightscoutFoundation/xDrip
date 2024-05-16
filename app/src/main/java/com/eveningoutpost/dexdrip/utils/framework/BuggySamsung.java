9e78be6150028d895f781a159802d496c99e1ec9
Navid <Navid200@users.noreply.github.com>
2024-05-16 14:16:18 -0400

    Huawei wake workaround

--- app/src/main/java/com/eveningoutpost/dexdrip/utils/framework/BuggySamsung.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/utils/framework/BuggySamsung.java
@@ -70,7 +70,8 @@ public class BuggySamsung {
         return Build.MANUFACTURER.toLowerCase().contains("samsung")
                 || Build.MANUFACTURER.toLowerCase().contains("xiaomi")
                 || Build.MANUFACTURER.toLowerCase().contains("oneplus")    // experimental test
-                || Build.MANUFACTURER.toLowerCase().contains("oppo");      // experimental test
+                || Build.MANUFACTURER.toLowerCase().contains("oppo")      // experimental test
+                || Build.MANUFACTURER.toLowerCase().contains("huawei");      // experimental test
     }
 
 }
