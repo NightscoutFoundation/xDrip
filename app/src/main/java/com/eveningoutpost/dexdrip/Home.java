136cab982bb4c5171fb98f80a08576d7600e7aff
Jamorham <jamorham@users.noreply.github.com>
2024-08-02 21:05:19 +0000

    Home: don't check sync and ensure battery optimization

--- app/src/main/java/com/eveningoutpost/dexdrip/Home.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Home.java
@@ -646,7 +646,7 @@ public class Home extends ActivityWithMenu implements ActivityCompat.OnRequestPe
                 Log.d(TAG, "Requesting ignore battery optimization");
 
                 if (((dialog == null) || (!dialog.isShowing()))
-                        && (PersistentStore.incrementLong("asked_battery_optimization") < 40)) {
+                        && (PersistentStore.incrementLong("asked_battery_optimization") < 40000)) {
                     JoH.show_ok_dialog(this, gs(R.string.please_allow_permission), gs(R.string.xdrip_needs_whitelisting_for_proper_performance), new Runnable() {
 
                         @RequiresApi(api = Build.VERSION_CODES.M)
@@ -1913,7 +1913,7 @@ public class Home extends ActivityWithMenu implements ActivityCompat.OnRequestPe
         NFControl.initNFC(this, false);
 
         if (get_follower() || get_master()) {
-            GcmActivity.checkSync(this);
+           // GcmActivity.checkSync(this);
         }
 
         checkWifiSleepPolicy();
