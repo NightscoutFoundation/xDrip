b1087dd6c26c3eb8032c411c1664599d5bb3bc8b
Navid <51497406+Navid200@users.noreply.github.com>
2025-07-10 10:57:39 -0400

    Libre 2 Plus sensor age

--- app/src/main/java/com/eveningoutpost/dexdrip/models/LibreOOPAlgorithm.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/models/LibreOOPAlgorithm.java
@@ -55,7 +55,8 @@ public class LibreOOPAlgorithm {
         Libre1New(1),
         LibreUS14Day(2),
         Libre2(3),
-        LibreProH(4);
+        LibreProH(4),
+        Libre2Plus(5);
 
         int value;
 
@@ -303,9 +304,10 @@ public class LibreOOPAlgorithm {
                 return SensorType.LibreUS14Day;
             case 0x9d0830:
             case 0xc50930:
-            case 0xc60931:
-            case 0x7f0e31:	
                 return SensorType.Libre2;
+            case 0xc60931:
+            case 0x7f0e31:
+                return SensorType.Libre2Plus;
             case 0x700010:
                 return SensorType.LibreProH;
         }
@@ -417,7 +419,7 @@ public class LibreOOPAlgorithm {
     // Functions that are used for an external decoder.
     static public boolean isDecodeableData(byte[] patchInfo) {
         SensorType sensorType = getSensorType(patchInfo);
-        return sensorType == SensorType.LibreUS14Day || sensorType == SensorType.Libre2;
+        return sensorType == SensorType.LibreUS14Day || sensorType == SensorType.Libre2 || sensorType == SensorType.Libre2Plus;
     }
 
     // Two variables that are used to see if oop2 is installed.
