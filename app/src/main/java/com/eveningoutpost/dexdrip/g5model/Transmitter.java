f162d70e180d93e61352e69ccb61cce801d93e67
Navid <51497406+Navid200@users.noreply.github.com>
2026-03-10 09:38:48 -0400

    Wrong TX ID notifications

--- app/src/main/java/com/eveningoutpost/dexdrip/g5model/Transmitter.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/g5model/Transmitter.java
@@ -1,5 +1,10 @@
 package com.eveningoutpost.dexdrip.g5model;
 
+import static com.eveningoutpost.dexdrip.services.Ob1G5CollectionService.getTransmitterID;
+import static com.eveningoutpost.dexdrip.utils.DexCollectionType.DexcomG5;
+
+import com.eveningoutpost.dexdrip.utils.DexCollectionType;
+
 /**
  * Created by joeginley on 3/19/16.
  */
@@ -13,4 +18,10 @@ public class Transmitter {
     public void authenticate() {
 
     }
+
+    // True if G6 is being used and the G6 transmitter ID has more than 6 characters and the last 2 are -1
+    public static boolean isDexTxIdEndingWithDash1() {
+        return DexCollectionType.getDexCollectionType() == DexcomG5 && getTransmitterID().length() > 6 && getTransmitterID().endsWith("-1");
+    }
+
 }
